0539aaeffa34b7d2a430058183428f25047843f2
Yasuhito Takamiya <yasuhito@gmail.com>
2011-05-25 10:20:30 +0900

    Add the spec logging API of Controller class.

---
+++ spec/trema/controller_spec.rb
@@ -0,0 +1,41 @@
+#
+# Author: Yasuhito Takamiya <yasuhito@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
+require File.join( File.dirname( __FILE__ ), "..", "spec_helper" )
+require "trema"
+
+
+module Trema
+  describe Controller do
+    it "should respond to logging methods" do
+      [ :critical, :error, :warn, :notice, :info, :debug ].each do | each |
+        Controller.new.__send__( each, "%s message", each ).should == "#{ each } message"
+      end
+    end
+  end
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
+
