23d361fc30a1cffe7d188bd4244c47664883d2e8
Yasuhito Takamiya <yasuhito@gmail.com>
2011-05-28 16:02:15 +0900

    Add yard comments

--- ruby/trema/app.rb
+++ ruby/trema/app.rb
@@ -1,6 +1,4 @@
 #
-# Trema application class.
-#
 # Author: Yasuhito Takamiya <yasuhito@gmail.com>
 #
 # Copyright (C) 2008-2011 NEC Corporation
@@ -25,36 +23,91 @@ require "trema/process"
 
 
 module Trema
+  #
+  # Trema applications
+  #
   class App < NetworkComponent
+    #
+    # Creates a new Trema application from {DSL::App}
+    #
+    # @example
+    #   app = Trema::App.new( stanza )
+    #
+    # @return [App]
+    #
+    # @api public
+    #
     def initialize stanza
       @stanza = stanza
       self.class.add self
     end
 
 
+    #
+    # Returns the name of application
+    #
+    # @example
+    #   app.name => "Trema Tetris"
+    #
+    # @return [String]
+    #
+    # @api public
+    #
     def name
       @stanza[ :name ]
     end
 
 
+    #
+    # Runs as a daemon
+    #
+    # @example
+    #   app.daemonize! => self
+    #
+    # @return [App]
+    #
+    # @api public
+    #
     def daemonize!
       if options
         sh "#{ command } -d #{ options.join " " }"
       else
         sh "#{ command } -d"
       end
+      self
     end
 
 
+    #
+    # Runs an application process
+    #
+    # @example
+    #   app.run! => self
+    #
+    # @return [App]
+    #
+    # @api public
+    #
     def run!
       if options
         sh "#{ command } #{ options.join " " }"
       else
         sh command
       end
+      self
     end
 
 
+    #
+    # Kills running application
+    #
+    # @example
+    #   app.shutdown!
+    #
+    # @return [undefined]
+    #
+    # @api public
+    #
     def shutdown!
       Trema::Process.read( pid_file, @name ).kill!
     end
@@ -65,16 +118,37 @@ module Trema
     ################################################################################
 
 
+    #
+    # Returns the path of pid file
+    #
+    # @return [String]
+    #
+    # @api private
+    #
     def pid_file
       File.join Trema.tmp, "#{ @name }.pid"
     end
 
 
+    #
+    # Returns application's command to execute
+    #
+    # @return [String]
+    #
+    # @api private
+    #
     def command
       "#{ @stanza[ :path ] } --name #{ name }"
     end
 
 
+    #
+    # Returns command line options
+    #
+    # @return [Array]
+    #
+    # @api private
+    #
     def options
       @stanza[ :options ]
     end
