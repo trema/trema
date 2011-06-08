fab6f0c8301658b76d89fc12b933d0f9dee25aa8
Yasuhito Takamiya <yasuhito@gmail.com>
2011-06-08 11:37:26 +0900

    sleep 1 after send_packets

--- send_packets_steps.rb
+++ send_packets_steps.rb
@@ -24,6 +24,11 @@ When /^I send (\d+) packets from (.+) to (.+)$/ do | n_packets, host_a, host_b |
 end
 
 
+When /^I send 1 packet from (.+) to (.+)$/ do | host_a, host_b |
+  When "I send 1 packets from #{ host_a } to #{ host_b }"
+end
+
+
 When /^I send packets from (.+) to (.+) \(duration = (\d+)\)$/ do | host_a, host_b, duration |
   run "./trema send_packets --source #{ host_a } --dest #{ host_b } --duration #{ duration }"
   sleep 1  # ensure that all packets are sent
