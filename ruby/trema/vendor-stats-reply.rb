70eddfd7928aeeff95b803d53cebcb445f2b03c9
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-07-18 09:58:23 +0900

    rename Stats class to StatsHelper

--- ruby/trema/vendor-stats-reply.rb
+++ ruby/trema/vendor-stats-reply.rb
@@ -1,18 +1,19 @@
-require "trema/stats"
+require "trema/stats-helper"
+
 module Trema
-	class VendorStatsReply < Stats
-		FIELDS = %w(vendor_id)
+  class VendorStatsReply < StatsHelper
+    FIELDS = %w(vendor_id)
 
-		FIELDS.each { |field| attr_reader field.intern }
+    FIELDS.each { |field| attr_reader field.intern }
 
-		NAME = self.name
+    NAME = self.name
 
     def initialize options
-			super FIELDS, options
-		end
+      super FIELDS, options
+    end
 
-		def to_s
-			str="#{NAME}\n" + super.to_s
-		end
-	end
+    def to_s
+      str="#{NAME}\n" + super.to_s
+    end
+  end
 end
