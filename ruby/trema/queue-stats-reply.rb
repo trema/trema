10b748c89c47e5a337d35158fe905e7fa1f91763
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-07-18 09:56:59 +0900

    rename Stats class to StatsHelper

--- ruby/trema/queue-stats-reply.rb
+++ ruby/trema/queue-stats-reply.rb
@@ -1,18 +1,19 @@
-require "trema/stats"
+require "trema/stats-helper"
+
 module Trema
-	class QueueStatsReply < Stats
-		FIELDS = %w(port_no queue_id tx_bytes tx_packets tx_errors)
+  class QueueStatsReply < StatsHelper
+    FIELDS = %w(port_no queue_id tx_bytes tx_packets tx_errors)
 
-		FIELDS.each { |field| attr_reader field.intern }
+    FIELDS.each { |field| attr_reader field.intern }
 
-		NAME = self.name
+    NAME = self.name
 
     def initialize options
-			super FIELDS, options
-		end
+      super FIELDS, options
+    end
 
-		def to_s
-			str="#{NAME}\n" + super.to_s
-		end
-	end
+    def to_s
+      str="#{NAME}\n" + super.to_s
+    end
+  end
 end
