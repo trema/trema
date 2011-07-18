7e0b7028d100367da18136633cd6dbcf8f2de281
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-07-18 09:54:35 +0900

    rename Stats class to StatsHelper

--- ruby/trema/aggregate-stats-reply.rb
+++ ruby/trema/aggregate-stats-reply.rb
@@ -1,19 +1,19 @@
-require "trema/stats"
+require "trema/stats-helper"
 
 module Trema
-	class AggregateStatsReply < Stats
-		FIELDS = %w(packet_count byte_count flow_count)
+  class AggregateStatsReply < StatsHelper
+    FIELDS = %w(packet_count byte_count flow_count)
 
-		FIELDS.each { |field| attr_reader field.intern }
+    FIELDS.each { |field| attr_reader field.intern }
 
-		NAME = self.name
+    NAME = self.name
 
     def initialize options
-			super FIELDS, options
-		end
+      super FIELDS, options
+    end
 
-		def to_s
-			str="#{NAME}\n" + super.to_s
-		end
-	end
+    def to_s
+      str="#{NAME}\n" + super.to_s
+    end
+  end
 end
