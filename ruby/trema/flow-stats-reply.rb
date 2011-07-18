a79aa09e3c54fffbe089175baada6fd523f47dc9
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-07-18 09:55:01 +0900

    rename Stats class to StatsHelper

--- ruby/trema/flow-stats-reply.rb
+++ ruby/trema/flow-stats-reply.rb
@@ -1,19 +1,20 @@
-require "trema/stats"
+require "trema/stats-helper"
+
 module Trema
-	class FlowStatsReply < Stats
-		FIELDS = %w(length table_id match duration_sec duration_nsec) + 
-			%w(priority idle_timeout hard_timeout cookie packet_count byte_count)
+  class FlowStatsReply < StatsHelper
+    FIELDS = %w(length table_id match duration_sec duration_nsec) + 
+      %w(priority idle_timeout hard_timeout cookie packet_count byte_count)
 
-		FIELDS.each { |field| attr_reader field.intern }
+    FIELDS.each { |field| attr_reader field.intern }
 
-		NAME = self.name
+    NAME = self.name
 
     def initialize options 
-			super FIELDS, options
-		end
+      super FIELDS, options
+    end
 
-		def to_s
-			str="#{NAME}\n" + super.to_s
-		end
-	end
+    def to_s
+      str="#{NAME}\n" + super.to_s
+    end
+  end
 end
