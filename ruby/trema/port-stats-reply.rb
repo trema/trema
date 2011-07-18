68c7d30a21ddffa593655bdef1bdbde14bebb6c6
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-07-18 09:56:25 +0900

    rename Stats class to StatsHelper

--- ruby/trema/port-stats-reply.rb
+++ ruby/trema/port-stats-reply.rb
@@ -1,20 +1,21 @@
-require "trema/stats"
+require "trema/stats-helper"
+
 module Trema
-	class PortStatsReply < Stats
-		FIELDS = %w(port_no rx_packets tx_packets rx_bytes) + 
-			%w(tx_bytes rx_dropped tx_dropped rx_errors tx_errors) +
-			%w(rx_frame_err rx_over_err rx_crc_err collisions )
+  class PortStatsReply < StatsHelper
+    FIELDS = %w(port_no rx_packets tx_packets rx_bytes) + 
+      %w(tx_bytes rx_dropped tx_dropped rx_errors tx_errors) +
+      %w(rx_frame_err rx_over_err rx_crc_err collisions )
 
-		FIELDS.each { |field| attr_reader field.intern }
+    FIELDS.each { |field| attr_reader field.intern }
 
-		NAME = self.name
+    NAME = self.name
 
     def initialize options 
-			super FIELDS, options
-		end
+      super FIELDS, options
+    end
 
-		def to_s
-			str="#{NAME}\n" + super.to_s
-		end
-	end
+    def to_s
+      str="#{NAME}\n" + super.to_s
+    end
+  end
 end
