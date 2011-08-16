67b362ea3a0a76066479afcef69999d7d0cd702d
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-16 16:38:14 +0900

    add yard doc support and exception for transaction id

--- ruby/trema/hello.c
+++ ruby/trema/hello.c
@@ -54,6 +54,9 @@ hello_init( int argc, VALUE *argv, VALUE self ) {
     xid = get_transaction_id();
   }
   else {
+    if ( NUM2INT( xid_ruby ) < 0 ) {
+      rb_raise( rb_eArgError, "Transaction ID must be >= 0" );
+    }
     xid = ( uint32_t ) NUM2UINT( xid_ruby );
   }
   ( ( struct ofp_header * ) ( hello->data ) )->xid = htonl( xid );
