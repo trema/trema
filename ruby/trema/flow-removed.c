8f0c8311e6ba42b41f3c127c60bec3659b3dfab4
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-19 13:33:21 +0900

    add yard documentation

--- ruby/trema/flow-removed.c
+++ ruby/trema/flow-removed.c
@@ -26,73 +26,186 @@ extern VALUE mTrema;
 VALUE cFlowRemoved;
 
 
+/*
+ * @overload initialize(options={})
+ *   @example 
+ *     FlowRemoved( 
+ *       :datapath_id => 0xabc,
+ *       :transaction_id => 0,
+ *       :match => Match,
+ *       :cookie => 123456789,
+ *       :priority => 65535,
+ *       :reason => 0,
+ *       :duration_sec => 1,
+ *       :duration_nsec => 783000000,
+ *       :idle_timeout => 1,
+ *       :packet_count => 1
+ *       :byte_count=> 64
+ *     )
+ * 
+ *   @param [Hash] options the options hash.
+ * 
+ *   @option options [Symbol] :datapath_id
+ *     message originator identifier.
+ * 
+ *   @option options [Symbol] :transaction_id
+ *     unsolicited message transaction_id equals zero.
+ * 
+ *   @option options [Symbol] :match
+ *     a {Match} object describing the flow copied from the corresponding 
+ *     +OPFT_FLOW_MOD(add)+ message.
+ * 
+ *   @option options [Symbol] :cookie
+ *     an opaque identifier copied from the corresponding +OPFT_FLOW_MOD(add)+ 
+ *     message.
+ * 
+ *   @option options [Symbol] :priority
+ *     the priority level of the flow copied from the corresponding 
+ *     +OPFT_FLOW_MOD(add)+ message.
+ * 
+ *   @option options [Symbol] :reason
+ *     the reason why the flow removed.
+ * 
+ *   @option options [Symbol] :duration_sec
+ *     the number of seconds flow was active.
+ * 
+ *   @option options [Symbol] :duration_nsec
+ *     the number of nanoseconds flow was active.
+ * 
+ *   @option options [Symbol] :idle_timeout
+ *     time elapsed in seconds before flow removed copied from the 
+ *     corresponding +OPFT_FLOW_MOD(add)+ message.
+ * 
+ *   @option options [Symbol] :packet_count
+ *     the accumulated total number of packets.
+ * 
+ *   @option options [Symbol] :byte_count
+ *     the accumulated total number of bytes.
+ * 
+ * @return [FlowRemoved] 
+ *   an object that encapsulates the +OFPT_FLOW_REMOVED+ openflow message.
+ */
 static VALUE
-flow_removed_init( VALUE self, VALUE attribute ) {
-  rb_iv_set( self, "@attribute", attribute );
+flow_removed_init( VALUE self, VALUE options ) {
+  rb_iv_set( self, "@attribute", options );
   return self;
 }
 
 
+/*
+ * Message originator identifier.
+ *
+ * @return [Number] the value of attribute datapath_id.
+ */
 static VALUE
 flow_removed_datapath_id( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "datapath_id" ) ) );
 }
 
 
+/*
+ * For this asynchronous message the transaction_id is set to zero.
+ * 
+ * @return [Number] the value of attribute transaction_id.
+ */
 static VALUE
 flow_removed_transaction_id( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "transaction_id" ) ) );
 }
 
 
+/*
+ * @return [Match] an object that encapsulates flow details.
+ */
 static VALUE
 flow_removed_match( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "match" ) ) );
 }
 
 
+/*
+ * An opaque identifier copied from the corresponding +OPFT_FLOW_MOD(add)+ message.
+ * 
+ * @return [Number] the value of attribute cookie.
+ */
 static VALUE
 flow_removed_cookie( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "cookie" ) ) );
 }
 
 
+/*
+ * The priority level of the flow copied from the corresponding 
+ * +OPFT_FLOW_MOD(add)+ message.
+ * 
+ * @return [Number] the value of attribute priority.
+ */
 static VALUE
 flow_removed_priority( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "priority" ) ) );
 }
 
 
+/*
+ * The reason why the flow removed.
+ * 
+ * @return [Number] the value of attribute reason.
+ */
 static VALUE
 flow_removed_reason( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "reason" ) ) );
 }
 
 
+/*
+ * The number of seconds flow was active.
+ * 
+ * @return [Number] the value of attribute duration_sec.
+ */
 static VALUE
 flow_removed_duration_sec( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "duration_sec" ) ) );
 }
 
 
+/*
+ * The number of nanoseconds flow was active.
+ * 
+ * @return [Number] the value of attribute duration_nsec.
+ */
 static VALUE
 flow_removed_duration_nsec( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "duration_nsec" ) ) );
 }
 
 
+/*
+ * Time elapsed in seconds before flow removed.
+ * 
+ * @return [Number] the value of attribute idle_timeout.
+ */
 static VALUE
 flow_removed_idle_timeout( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "idle_timeout" ) ) );
 }
 
 
+/*
+ * The accumulated total number of packets.
+ * 
+ * @return [Number] the value of attribute packet_count.
+ */
 static VALUE
 flow_removed_packet_count( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "packet_count" ) ) );
 }
 
 
+/*
+ * The accumulated total number of bytes.
+ * 
+ * @return [Number] the value of attribute byte_count.
+ */
 static VALUE
 flow_removed_byte_count( VALUE self ) {
   return rb_hash_aref( rb_iv_get( self, "@attribute" ), ID2SYM( rb_intern( "byte_count" ) ) );
