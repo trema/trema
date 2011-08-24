6528f453a10d494518f66be2906cb1da064f5240
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-24 13:19:17 +0900

    add yard documentation

--- ruby/trema/aggregate-stats-reply.rb
+++ ruby/trema/aggregate-stats-reply.rb
@@ -7,13 +7,33 @@ module Trema
     FIELDS.each { |field| attr_reader field.intern }
 
 
+    # Aggregate counters for multiple flows.
+    # A user would not implicitly instantiate a {AggregateStatsReply} object but
+    # would be created as a result of parsing the +OFPT_STATS_REPLY(OFPST_AGGREGATE) 
+    # message.
+    # 
+    # @overload initialize(options={})
+    # 
+    #   @example 
+    #     AggregateStatsReply.new(
+    #       :packet_count => 10,
+    #       :byte_count => 640,
+    #       :flow_count => 2
+    #     )
+    #   
+    #   @param [Hash] options the options hash.
+    #   
+    #   @option options [Symbol] :packet_count
+    #     the total packet counter.
+    #  
+    #   @option options [Symbol] :byte_count
+    #     the total byte counter.
     #
-    # Create AggregateStatsReply from options hash.
-    #
-    # @example AggregateStatsReply.new( options = {} )
-    #   options = { :packet_count => 1, :byte_count => 64, :flow_count => 1 }
-    #
-    # @return [AggregateStatsReply] an object that encapsulates the OFPST_STATS_REPLY(OFPST_AGGREGATE) OpenFlow message.
+    #   @option options [Symbol] :flow_count
+    #     the total flow counter.
+    #     
+    # @return [AggregateStatsReply] 
+    #   an object that encapsulates the OFPST_STATS_REPLY(OFPST_AGGREGATE) openFlow message.
     #
     def initialize options
       super FIELDS, options
