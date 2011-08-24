1f695ab87ab253a2f310bdd72ed505bb354a882d
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-24 13:22:18 +0900

    add yard documentation

--- ruby/trema/vendor-stats-reply.rb
+++ ruby/trema/vendor-stats-reply.rb
@@ -1,24 +1,32 @@
 require "trema/stats-helper"
 
 module Trema
-  #
-  # The vendor stats reply class
-  #
   class VendorStatsReply < StatsHelper
     FIELDS = %w(vendor_id)
 
     FIELDS.each { |field| attr_reader field.intern }
 
     
-   #
-   # Sets the attribute vendor_id to a passed in argument
-   # 
-   # @example VendorStatsReply.new 0x1234
-   #
-   # @return [VendorStatsReply]
-   # 
-   # @api public
-   #
+    # Vendor statistics reply.
+    # A user would not implicitly instantiate a {VendorStatsReply} object but would
+    # be created as a result of parsing the +OFPT_STATS_REPLY(OFPST_VENDOR)+
+    # openflow message.
+    #
+    # @overload initialize(otions={})
+    #
+    #   @example 
+    #     VendorStatsReply.new(
+    #       :vendor_id => 123
+    #     )
+    #
+    #   @param [Hash] options the options hash.
+    #
+    #   @option options [Symbol] :vendor_id
+    #     the specific vendor identifier.
+    #
+    # @return [VendorStatsReply] 
+    #   an object that encapsulates the OFPST_STATS_REPLY(OPPST_VENDOR) openFlow message. 
+    #
     def initialize options
       super FIELDS, options
     end
