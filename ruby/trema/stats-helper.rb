96d1c66208609894b71bfce0d96255182e989cf3
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-24 13:21:50 +0900

    add yard documentation

--- ruby/trema/stats-helper.rb
+++ ruby/trema/stats-helper.rb
@@ -1,6 +1,19 @@
 module Trema
   class StatsHelper
 
+    
+    # Called by each StatsReply subclass to set their instance attributes to a value.
+    # 
+    # @overload initialize(fields, options={})
+    # 
+    # @param [Array] fields
+    #   an array of attribute names to set.
+    # 
+    # @param [Hash] options
+    #   key/value pairs of attributes.
+    #   
+    # @return [void]
+    #
     def initialize fields, options
       fields.each do |field|
         instance_variable_set( "@#{field}", options[field.intern] )
@@ -8,6 +21,10 @@ module Trema
     end
 
     
+    #
+    # @return [String] 
+    #   an alphabetically sorted string of attribute name/value pairs.
+    #
     def to_s
       str = super.to_s + "\n"
       instance_variables.sort.each do |var|
