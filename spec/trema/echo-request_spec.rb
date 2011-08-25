6bd4259b019fac542082344d1f034a382f0487f9
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-25 13:32:53 +0900

    refactor RSpec #echo_request

--- spec/trema/echo-request_spec.rb
+++ spec/trema/echo-request_spec.rb
@@ -24,36 +24,28 @@ require "trema"
 
 describe EchoRequest do
   context "when an instance is created" do  
-    it "should automatically allocate a transaction ID" do
-      echo_request = EchoRequest.new( )
-      echo_request.transaction_id.should be_a_kind_of( Integer )
-      echo_request.transaction_id.should >= 0
-    end
-    
-    
-    it "should have no user data" do
-      EchoRequest.new.user_data.should be_nil
-    end
-    
-    
-    it "should have a valid transaction ID" do
-      EchoRequest.new( 1234 ).transaction_id.should == 1234
-    end
-    
-    it "should have a user data as specified" do
-      EchoRequest.new( 1234, "this is a test" ).user_data.should match( /this is a test/ )
-    end
-    
-    it "should raise an error if specified user data is not a string" do
-      expect {
-        EchoRequest.new( 1234, 456 )
-      }.to raise_error( ArgumentError )
-    end
-  end  
-
+    its ( :transaction_id ) { should be_a_kind_of( Integer ) }
+    its ( :transaction_id ) { should >= 0 }
+    its ( :user_data ) { should be_nil }
+  end
+  
+  
+  context "when an instance is created with arguments" do
+    subject { EchoRequest.new( 1234, "this is a test") }
+    its (:transaction_id ) { should == 1234 }
+    its (:user_data ) { should match( /this is a test/ ) }
+  end
+  
+  
+  it "should raise an error if user data is not a string" do
+    expect {
+      EchoRequest.new( 1234, 456 )
+    }.to raise_error( ArgumentError )
+  end
+  
   
   context "when #echo_request is sent" do
-    it "should receive #echo_reply" do
+    it "should #echo_reply" do
       class EchoRequestController < Controller; end
       network {
         vswitch( "echo_request" ) { datapath_id 0xabc }
