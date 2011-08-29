0e0231dfd2e5c33517cc4360c918fccf103d9f95
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-29 14:32:33 +0900

    add header/footer documentation

--- ruby/trema/table-stats-reply.rb
+++ ruby/trema/table-stats-reply.rb
@@ -1,10 +1,29 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 require "trema/stats-helper"
+
 
 module Trema
   class TableStatsReply < StatsHelper
     FIELDS = %w(table_id name wildcards max_entries ) +
       %w(active_count lookup_count matched_count)
-
     FIELDS.each { |field| attr_reader field.intern }
 
 
@@ -58,3 +77,10 @@ module Trema
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
