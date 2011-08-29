fe93f9635282d997cceb8264baf66db65598eaaa
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-29 14:32:55 +0900

    add header/footer documentation

--- ruby/trema/vendor-stats-reply.rb
+++ ruby/trema/vendor-stats-reply.rb
@@ -1,4 +1,25 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 require "trema/stats-helper"
+
 
 module Trema
   #
@@ -6,7 +27,6 @@ module Trema
   #
   class VendorStatsReply < StatsHelper
     FIELDS = %w(vendor_id)
-
     FIELDS.each { |field| attr_reader field.intern }
 
     
@@ -24,3 +44,10 @@ module Trema
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
