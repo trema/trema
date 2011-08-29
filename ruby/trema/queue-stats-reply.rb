a689cf3da334ef14825394ac1be6ff9f561a5b6f
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-29 14:31:59 +0900

    add header/footer documentation

--- ruby/trema/queue-stats-reply.rb
+++ ruby/trema/queue-stats-reply.rb
@@ -1,4 +1,25 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 require "trema/stats-helper"
+
 
 module Trema
   #
@@ -7,7 +28,6 @@ module Trema
   #
   class QueueStatsReply < StatsHelper
     FIELDS = %w(port_no queue_id tx_bytes tx_packets tx_errors)
-
     FIELDS.each { |field| attr_reader field.intern }
 
 
@@ -16,3 +36,10 @@ module Trema
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
