a1dae2a046d160e56c5d90d6d68bf26e28b07c69
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-29 14:46:33 +0900

    add header/footer documentation

--- ruby/trema/stats-helper.rb
+++ ruby/trema/stats-helper.rb
@@ -1,3 +1,23 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 module Trema
   class StatsHelper
 
@@ -23,7 +43,7 @@ module Trema
     
     #
     # @return [String] 
-    #   an alphabetically sorted string of attribute name/value pairs.
+    #   an alphabetically sorted text of attribute name/value pairs.
     #
     def to_s
       str = super.to_s + "\n"
@@ -34,3 +54,10 @@ module Trema
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
