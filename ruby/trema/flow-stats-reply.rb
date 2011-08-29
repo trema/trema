307d5b95d804b2ee87a9ab376fb7e3095f8f4347
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-08-29 14:30:19 +0900

    add header/footer documentation

--- ruby/trema/flow-stats-reply.rb
+++ ruby/trema/flow-stats-reply.rb
@@ -1,3 +1,23 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 require "trema/stats-helper"
 
 
@@ -5,7 +25,6 @@ module Trema
   class FlowStatsReply < StatsHelper
     FIELDS = %w(length table_id match duration_sec duration_nsec) + 
       %w(priority idle_timeout hard_timeout cookie packet_count byte_count actions)
-
     FIELDS.each { |field| attr_reader field.intern }
 
     
@@ -80,3 +99,10 @@ module Trema
     end
   end
 end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
