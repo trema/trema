c2d60fe754bcc69c06ab42d0d4e636c9d2ebd78f
Yasuhito Takamiya <yasuhito@gmail.com>
2011-08-29 15:58:49 +0900

    swap to correct implicitly to explicitly
    Conflicts:
    
    	ruby/trema/flow-stats-reply.rb

--- ruby/trema/flow-stats-reply.rb
+++ ruby/trema/flow-stats-reply.rb
@@ -27,14 +27,68 @@ module Trema
     FIELDS.each { | each | attr_reader each.intern }
 
 
+    # Flow counters for one or more matched flows.
+    # A user would not explicitly instantiate a {FlowStatsReply} object but 
+    # would be created as a result of parsing the +OFPT_STATS_REPLY(OFPST_FLOW)+ 
+    # message.
     #
-    # Creates a FlowStatsReply object from options hash.
-    #
-    # @example FlowStatsReply.new( options = {} )
-    #   options { :length => 86, :table_id => 1, :match => Match.new
-    #   :duration_sec => 10, :duration_nsec => 555, :priority => 0, :idle_timeout => 0,
-    #   :hard_timeout => 0, :cookie => 0xabcd, :packet_count => 1, :byte_count => 1
-    #   :actions => [ ActionOutput.new ] }
+    # @overload initialize(options={})
+    # 
+    #   @example 
+    #     FlowStatsReply.new(
+    #       :length => 96, 
+    #       :table_id => 0, 
+    #       :match => Match.new
+    #       :duration_sec => 10, 
+    #       :duration_nsec => 106000000, 
+    #       :priority => 0, 
+    #       :idle_timeout => 0,
+    #       :hard_timeout => 0, 
+    #       :cookie => 0xabcd, 
+    #       :packet_count => 1, 
+    #       :byte_count => 1
+    #       :actions => [ ActionOutput.new ]
+    #     )
+    #   
+    #   @param [Hash] options the options hash.
+    #   
+    #   @option options [Symbol] :length
+    #     the length of this packet.
+    #   
+    #   @option options [Symbol] :table_id
+    #     set to zero.
+    #     
+    #   @option options [Symbol] :match
+    #     Match object describing flow fields.
+    #     
+    #   @option options [Symbol] :duration_sec
+    #     the time in seconds the flow been active.
+    #   
+    #   @option options [Symbol] :duration_nsec
+    #     the time in nanosecs the flow been active.
+    #   
+    #   @option options [Symbol] :priority
+    #     the priority of the flow.
+    #   
+    #   @option options [Symbol] :idle_timeout
+    #     an inactivity time in seconds before the flow is deleted. Zero means
+    #     no deletion.
+    #     
+    #   @option options [Symbol] :hard_timeout
+    #     a fixed time interval before the flow is deleted. Zero means no 
+    #     deletion.
+    #   
+    #   @option options [Symbol] :cookie
+    #     an opaque identifier used as a unique key to match flow entries.
+    #   
+    #   @option options [Symbol] :packet_count
+    #     count of the number of packets matched the flow.
+    #   
+    #   @option options [Symbol] :byte_count
+    #     count of the number of bytes matched the flow.
+    #   
+    #   @option options [Symbol] :actions
+    #     an array of action objects for the flow.
     #
     # @return [FlowStatsReply] an object that encapsulates the OFPST_STATS_REPLY (OPPST_FLOW) OpenFlow message.
     #
