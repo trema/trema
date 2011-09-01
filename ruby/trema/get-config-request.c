b471a9064ad97bd6bcaf1504d9b0e9d462c2c2cc
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-09-01 11:43:19 +0900

    add yard documentation

--- ruby/trema/get-config-request.c
+++ ruby/trema/get-config-request.c
@@ -28,34 +28,53 @@ VALUE cGetConfigRequest;
 
 static VALUE
 get_config_request_alloc( VALUE klass ) {
-  buffer *get_config_request = create_get_config_request( get_transaction_id( ) );
-
+  buffer *get_config_request = create_get_config_request( get_transaction_id() );
   return Data_Wrap_Struct( klass, NULL, free_buffer, get_config_request );
 }
 
 
+/*
+ * Creates a {GetConfigRequest} instance to query configuration parameters 
+ * from the switch.
+ *
+ * @overload initialize(transaction_id=nil)
+ *   Creates a {GetConfigRequest} object by specifying its transaction id. If
+ *   transaction_id is not specified, an auto-generated transaction_id
+ *   is set.
+ *
+ * @raise [ArgumentError] if transaction id is negative.
+ *
+ * @return [GetConfigRequest] an object that encapsulates the +OFPT_GET_CONFIG+ openflow message.
+ */
 static VALUE
 get_config_request_init( int argc, VALUE *argv, VALUE self ) {
   buffer *get_config_request;
-
   Data_Get_Struct( self, buffer, get_config_request );
-  VALUE xid_ruby;
+  
   uint32_t xid;
-
+  VALUE xid_ruby;
   if ( rb_scan_args( argc, argv, "01", &xid_ruby ) == 1 ) {
+    if ( NUM2INT( xid_ruby ) < 0 ) {
+      rb_raise( rb_eArgError, "Transaction ID must be >= 0" );
+    }
     xid = ( uint32_t ) NUM2UINT( xid_ruby );
-  } else {
-    xid = get_transaction_id( );
+  } 
+  else {
+    xid = get_transaction_id();
   }
   ( ( struct ofp_header * ) ( get_config_request->data ) )->xid = htonl( xid );
   return self;
 }
 
 
+/*
+ * Transaction ids, message sequence numbers matching requests to replies.
+ *
+ * @return [Number] the value of attribute transaction id.
+ */
 static VALUE
 get_config_request_transaction_id( VALUE self ) {
   buffer *get_config_request;
-
   Data_Get_Struct( self, buffer, get_config_request );
   uint32_t xid = ntohl( ( ( struct ofp_header * ) ( get_config_request->data ) )->xid );
   return UINT2NUM( xid );
@@ -63,7 +82,7 @@ get_config_request_transaction_id( VALUE self ) {
 
 
 void
-Init_get_config_request( ) {
+Init_get_config_request() {
   cGetConfigRequest = rb_define_class_under( mTrema, "GetConfigRequest", rb_cObject );
   rb_define_alloc_func( cGetConfigRequest, get_config_request_alloc );
   rb_define_method( cGetConfigRequest, "initialize", get_config_request_init, -1 );
