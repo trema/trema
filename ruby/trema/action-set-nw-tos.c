0d9cd14aa0269714c110203e2833b77bfc8861c9
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-09-03 10:12:38 +0900

    add yard documentation

--- ruby/trema/action-set-nw-tos.c
+++ ruby/trema/action-set-nw-tos.c
@@ -26,6 +26,19 @@ extern VALUE mTrema;
 VALUE cActionSetNwTos;
 
 
+/*
+ * An action to modify the IP ToS/DSCP field of a packet.
+ *
+ * @overload initialize(nw_tos)
+ *
+ * @param [Number] nw_tos
+ *  the ToS/DSCP field to set to.
+ *
+ * @raise [ArgumentError] if nw_tos argument is not supplied.
+ *
+ * @return [ActionSetNwTos]
+ *   an object that encapsulates this action.
+ */
 static VALUE
 action_set_nw_tos_init( VALUE self, VALUE nw_tos ) {
   rb_iv_set( self, "@nw_tos", nw_tos );
@@ -33,36 +46,46 @@ action_set_nw_tos_init( VALUE self, VALUE nw_tos ) {
 }
 
 
+/*
+ * The ToS/DSCP value to set to.
+ *
+ * @return [Number] the value of attribute nw_tos.
+ */
 static VALUE
 action_get_nw_tos( VALUE self ) {
   return rb_iv_get( self, "@nw_tos" );
 }
 
 
+/*
+ * Appends its action(nw_tos) to the list of actions.
+ *
+ * @return [ActionSetNwTos] self
+ */
 static VALUE
 action_set_nw_tos_append( VALUE self, VALUE action_ptr ) {
   openflow_actions *actions;
-  uint8_t nw_tos = ( uint8_t ) NUM2UINT( action_get_nw_tos( self ) );
-
   Data_Get_Struct( action_ptr, openflow_actions, actions );
+  uint8_t nw_tos = ( uint8_t ) NUM2UINT( action_get_nw_tos( self ) );
   append_action_set_nw_tos( actions, nw_tos );
-
   return self;
 }
 
 
+/*
+ * (see ActionEnqueue#to_s)
+ */
 static VALUE
 action_set_nw_tos_to_s( VALUE self ) {
   char str[ 64 ];
   uint8_t nw_tos = ( uint8_t ) NUM2UINT( action_get_nw_tos( self ) );
-
   sprintf( str, "#<%s> nw_tos = %u", rb_obj_classname( self ), nw_tos );
   return rb_str_new2( str );
 }
 
 
 void
-Init_action_set_nw_tos( ) {
+Init_action_set_nw_tos() {
   cActionSetNwTos = rb_define_class_under( mTrema, "ActionSetNwTos", rb_cObject );
   rb_define_method( cActionSetNwTos, "initialize", action_set_nw_tos_init, 1 );
   rb_define_method( cActionSetNwTos, "nw_tos", action_get_nw_tos, 0 );
