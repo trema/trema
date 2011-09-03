a171bfa55063d3ca5f1cf51d9238e73007e465d6
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-09-03 09:13:08 +0900

    fix coding style

--- spec/trema/action-set-dl-dst_spec.rb
+++ spec/trema/action-set-dl-dst_spec.rb
@@ -22,10 +22,10 @@ require File.join( File.dirname( __FILE__ ), "..", "spec_helper" )
 require "trema"
 
 
-describe Trema::ActionSetDlDst do
+describe ActionSetDlDst do
   context "when an instance is created" do
-    subject { Trema::ActionSetDlDst.new( Mac.new( "52:54:00:a8:ad:8c" ) ) }
-    its( :dl_dst ) { should be_an_instance_of( Trema::Mac ) }
+    subject { ActionSetDlDst.new( Mac.new( "52:54:00:a8:ad:8c" ) ) }
+    its( :dl_dst ) { should be_an_instance_of( Mac ) }
     it { should respond_to( :to_s ) }
     it "should print its attributes" do
       subject.to_s.should == "#<Trema::ActionSetDlDst> dl_dst = 52:54:00:a8:ad:8c"
@@ -33,7 +33,7 @@ describe Trema::ActionSetDlDst do
     
     
     it "should append its action to a list of actions" do
-      openflow_actions = double( )
+      openflow_actions = double()
       subject.should_receive( :append ).with( openflow_actions )
       subject.append( openflow_actions )
     end
@@ -42,7 +42,7 @@ describe Trema::ActionSetDlDst do
     context "when dl_dst is not supplied" do
       it "should raise an error" do
         lambda do
-          Trema::ActionSetDlDst.new( )
+          ActionSetDlDst.new( )
         end.should raise_error( ArgumentError )
       end
     end
@@ -51,7 +51,7 @@ describe Trema::ActionSetDlDst do
     context "when dl_dst is not a Trema::Mac object" do  
       it "should raise an error" do
         lambda do
-          Trema::ActionSetDlDst.new( 1234 )
+          ActionSetDlDst.new( 1234 )
         end.should raise_error( ArgumentError, /dl dst address should be a Mac object/ )
       end
     end
