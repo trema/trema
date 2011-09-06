aeb1aad3bf261d56262023abd6f45f671660e90a
Yasuhito Takamiya <yasuhito@gmail.com>
2011-09-06 09:28:43 +0900

    fix coding style.

--- spec/trema/action-set-dl-dst_spec.rb
+++ spec/trema/action-set-dl-dst_spec.rb
@@ -26,13 +26,13 @@ describe ActionSetDlDst do
   context "when an instance is created" do
     subject { ActionSetDlDst.new( Mac.new( "52:54:00:a8:ad:8c" ) ) }
     its( :dl_dst ) { should be_an_instance_of( Mac ) }
-    it { should respond_to( :to_s ) }
     it "should print its attributes" do
       subject.to_s.should == "#<Trema::ActionSetDlDst> dl_dst = 52:54:00:a8:ad:8c"
     end
-    
+
+
     it "should append its action to a list of actions" do
-      openflow_actions = double()
+      openflow_actions = double
       subject.should_receive( :append ).with( openflow_actions )
       subject.append( openflow_actions )
     end
@@ -41,7 +41,7 @@ describe ActionSetDlDst do
     context "when dl_dst is not supplied" do
       it "should raise an error" do
         lambda do
-          ActionSetDlDst.new( )
+          ActionSetDlDst.new
         end.should raise_error( ArgumentError )
       end
     end
