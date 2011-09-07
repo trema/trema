3494eedc617645380d1d808397863598fa8c096e
Shreyansh Jain <shreyansh.jain@nechclst.in>
2011-09-07 17:49:39 +0530

    Updated openflow_service_interface.h file for Doxygen enabled comments.

--- src/lib/openflow_service_interface.h
+++ src/lib/openflow_service_interface.h
@@ -19,14 +19,22 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+/**
+ * @file
+ *
+ * @brief OpenFlow service interface declarations
+ *
+ * Message type and header definitions for sending/receiving OpenFlow messages
+ * via messenger
+ *
+ */
 
 #ifndef OPENFLOW_SERVICE_INTERFACE_H
 #define OPENFLOW_SERVICE_INTERFACE_H
 
 
-/**
- * Message type definitions for sending/receiving OpenFlow messages or
- * related events via messenger.
+/*
+ *  Message type definitions for sending/receiving OpenFlow messages or related events via messenger.
  */
 #define MESSENGER_OPENFLOW_MESSAGE 1
 #define MESSENGER_OPENFLOW_CONNECTED 2
@@ -36,11 +44,11 @@
 
 
 /**
- * Header for sending/receiving OpenFlow messages or events via messenger.
- * A null-terminated service name can be provided after service_name_len
+ * Header definitions for sending/receiving OpenFlow messages or events via
+ * messenger. A null-terminated service name can be provided after service_name_length
  * and an OpenFlow message must be included in the rest of part in case of
- * MESSENGER_OPENFLOW_MESSAGE. service_name_length can be zero if service
- * name notification is not necessary.
+ * MESSENGER_OPENFLOW_MESSAGE. service_name_length can be zero if service name
+ * notification is not necessary.
  */
 typedef struct openflow_service_header {
   uint64_t datapath_id;
