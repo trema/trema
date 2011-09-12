3dcfdd2747c4df3758842cceff6bedb4ffcb8438
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-09-12 13:02:03 +0900

    change #to_s to #inspect

--- spec/trema/action-set-tp-dst_spec.rb
+++ spec/trema/action-set-tp-dst_spec.rb
@@ -26,13 +26,12 @@ describe ActionSetTpDst do
   context "when an instance is created" do
     subject { ActionSetTpDst.new( 5555 ) }
     its( :tp_dst ) { should == 5555 }
-    it { should respond_to( :to_s ) }
     it "should print its attributes" do
-      subject.to_s.should == "#<Trema::ActionSetTpDst> tp_port = 5555"
+      subject.inspect.should == "#<Trema::ActionSetTpDst tp_port=5555>"
     end
     
     it "should append its action to a list of actions" do
-      openflow_actions = double()
+      openflow_actions = double
       subject.should_receive( :append ).with( openflow_actions )
       subject.append( openflow_actions )
     end
