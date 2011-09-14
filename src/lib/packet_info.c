e3abb2e08ad5fffcf6a17f3516f47cd67baf7853
kazuya <kazuya@yayoi.(none)>
2011-09-14 09:28:27 +0900

    Add packet_type functions.

--- src/lib/packet_info.c
+++ src/lib/packet_info.c
@@ -1,5 +1,5 @@
 /*
- * Author: Naoyoshi Tada
+ * Author: Kazuya Suzuki, Naoyoshi Tada
  *
  * Copyright (C) 2008-2011 NEC Corporation
  *
@@ -24,9 +24,11 @@
  * IPv4 packet, TCP packet, UDP packet, ICMP packet.
  */
 
+
 #include <assert.h>
 #include "packet_info.h"
 #include "wrapper.h"
+#include "trema.h"
 
 
 /**
@@ -59,12 +61,12 @@ void
 alloc_packet_info( buffer *buf ) {
   assert( buf != NULL );
 
-  packet_info *packet_info = xcalloc( 1, sizeof( packet_info ) );
-  assert( packet_info != NULL );
+  void *user_data = xcalloc( 1, sizeof( packet_info ) );
+  assert( user_data != NULL );
 
-  memset( packet_info, 0, sizeof( packet_info ) );
+  memset( user_data, 0, sizeof( packet_info ) );
 
-  buf->user_data = packet_info;
+  buf->user_data = user_data;
   buf->user_data_free_function = free_packet_info;
 }
 
@@ -108,6 +110,246 @@ free_packet( buffer *buf ) {
   free_buffer( buf );
 }
 
+
+/**
+ * Checks whether packet type is dix or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is dix
+ */
+static bool
+if_packet_type( const buffer *frame, uint32_t type ) {
+  assert( frame != NULL );
+  assert( frame->user_data != NULL );
+
+  packet_info *packet_info0 = ( packet_info * )frame->user_data;
+  
+  return ( ( packet_info0->format & type ) != 0 );
+  
+}
+
+
+/**
+ * Checks whether packet type is dix or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is dix
+*/
+bool packet_type_eth_dix( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return if_packet_type( frame, ETH_DIX );
+}
+
+
+/**
+ * Checks whether packet has vtag or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet has vtag
+ */
+bool packet_type_eth_vtag( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return if_packet_type( frame, ETH_8021Q );
+}
+
+
+/**
+ * Checks whether packet type is 802.3 raw or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 raw
+ */
+bool packet_type_eth_raw( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return if_packet_type( frame, ETH_8023_RAW );
+}
+
+
+/**
+ * Checks whether packet type is 802.3 llc or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 llc
+ */
+bool packet_type_eth_llc( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return if_packet_type( frame, ETH_8023_LLC );
+}
+
+
+/**
+ * Checks whether packet type is 802.3 snap or not.
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 snap
+ */
+bool packet_type_eth_snap( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return if_packet_type( frame, ETH_8023_SNAP );
+}
+
+
+/**
+ * Checks whether packet type is dix with vtag
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is dix with vtag
+ */
+bool packet_type_eth_vtag_dix( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  if ( if_packet_type( frame, ETH_DIX ) 
+       & if_packet_type( frame, ETH_8021Q ) ) 
+    return true;
+  else 
+    return false;
+}
+
+
+/**
+ * Checks whether packet type is 802.3 raw with vtag
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 raw with vtag
+ */
+bool packet_type_eth_vtag_raw( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  if ( if_packet_type( frame, ETH_8023_RAW ) 
+       & if_packet_type( frame, ETH_8021Q ) ) 
+    return true;
+  else 
+    return false;
+}
+
+
+/**
+ * Checks whether packet type is 802.3 llc with vtag
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 llc with vtag
+ */
+bool packet_type_eth_vtag_llc( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  if ( if_packet_type( frame, ETH_8023_LLC ) 
+       & if_packet_type( frame, ETH_8021Q ) ) 
+    return true;
+  else 
+    return false;
+}
+
+
+/**
+ * Checks whether packet type is 802.3 snap with vtag
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is 802.3 snap with vtag
+ */
+bool packet_type_eth_vtag_snap( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  if ( if_packet_type( frame, ETH_8023_SNAP ) 
+       & if_packet_type( frame, ETH_8021Q ) ) 
+    return true;
+  else 
+    return false;
+}
+
+
+/**
+ * Checks whether packet type is ARP
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is arp
+ */
+bool packet_type_eth_arp( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return ( if_packet_type( frame, NW_ARP ) );
+}
+
+
+/**
+ * Checks whether packet type is IPv4
+ * @param buf Pointer to buffer type structure
+ * @return bool true if packet type is ipv4
+ */
+bool packet_type_eth_ipv4( const buffer *frame ) {
+  if ( frame == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+  if ( frame->user_data == NULL ) {
+    die( "illegal argument to %s", __func__ );
+  }
+
+  return ( if_packet_type( frame, NW_IPV4 ) );
+}
+
+#if 0
+bool packet_type_eth_ipv4_icmpv4( const buffer *frame );
+bool packet_type_eth_ipv4_tcp( const buffer *frame );
+bool packet_type_eth_ipv4_udp( const buffer *frame );
+bool packet_type_eth_vtag_arp( const buffer *frame );
+bool packet_type_eth_vtag_ipv4( const buffer *frame );
+bool packet_type_eth_vtag_ipv4_icmpv4( const buffer *frame );
+bool packet_type_eth_vtag_ipv4_tcp( const buffer *frame );
+bool packet_type_eth_vtag_ipv4_udp( const buffer *frame );
+bool packet_type_eth_snap_arp( const buffer *frame );
+bool packet_type_eth_snap_ipv4( const buffer *frame );
+bool packet_type_eth_snap_ipv4_icmpv4( const buffer *frame );
+bool packet_type_eth_snap_ipv4_tcp( const buffer *frame );
+bool packet_type_eth_snap_ipv4_udp( const buffer *frame );
+bool packet_type_eth_snap_vtag_arp( const buffer *frame );
+bool packet_type_eth_snap_vtag_ipv4( const buffer *frame );
+bool packet_type_eth_snap_vtag_ipv4_icmpv4( const buffer *frame );
+bool packet_type_eth_snap_vtag_ipv4_tcp( const buffer *frame );
+bool packet_type_eth_snap_vtag_ipv4_udp( const buffer *frame );
+#endif
 
 /*
  * Local variables:
