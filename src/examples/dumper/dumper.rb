c3ba20e5d1619354ebb6daee73be71f493430f10
Yasuhito Takamiya <yasuhito@gmail.com>
2011-10-04 09:28:12 +0900

    Refactoring dumper.rb: use #{ ... } to output message properties.

--- src/examples/dumper/dumper.rb
+++ src/examples/dumper/dumper.rb
@@ -24,11 +24,11 @@ class Dumper < Controller
   def packet_in datapath_id, message
     info "[packet_in]"
     info "datapath_id: #{ datapath_id.to_hex }"
-    info "buffer_id: %#x" % message.buffer_id
-    info "total_len: %u" % message.total_len
-    info "in_port: %u" % message.in_port
-    info "reason: %#x" % message.reason
-    info "data: #{ message.data.unpack( "H*" ) }"
+    info "buffer_id: #{ message.buffer_id.to_hex }"
+    info "total_len: #{ message.total_len }"
+    info "in_port: #{ message.in_port }"
+    info "reason: #{ message.reason.to_hex }"
+    info "data: #{ message.data.unpack "H*" }"
     @datapath_id, @match, @in_port = datapath_id, Match.from( message ), message.in_port
   end
 
@@ -36,31 +36,31 @@ class Dumper < Controller
   def flow_removed datapath_id, message
     info "[flow removed]"
     info "datapath_id: #{ datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
+    info "transaction_id: #{ message.transaction_id.to_hex }"
 
     info "match:"
-    info "  wildcards: %#x" % message.match.wildcards
-    info "  in_port: %u" % message.match.in_port
-    info "  dl_src: #{message.match.dl_src.to_s}"
-    info "  dl_dst: #{message.match.dl_dst.to_s}"
-    info "  dl_vlan: %u" % message.match.dl_vlan
-    info "  dl_vlan_pcp: %u" % message.match.dl_vlan_pcp
-    info "  dl_type: %#x" % message.match.dl_type
-    info "  nw_tos: %u" % message.match.nw_tos
-    info "  nw_proto: %#x" % message.match.nw_proto
-    info "  nw_src: %#x" % message.match.nw_src
-    info "  nw_dst: %#x" % message.match.nw_dst
-    info "  tp_src: %u" % message.match.tp_src
-    info "  tp_dst: %u" % message.match.tp_dst
+    info "  wildcards: #{ message.match.wildcards.to_hex }"
+    info "  in_port: #{ message.match.in_port }"
+    info "  dl_src: #{ message.match.dl_src }"
+    info "  dl_dst: #{ message.match.dl_dst }"
+    info "  dl_vlan: #{ message.match.dl_vlan }"
+    info "  dl_vlan_pcp: #{ message.match.dl_vlan_pcp }"
+    info "  dl_type: #{ message.match.dl_type.to_hex }"
+    info "  nw_tos: #{ message.match.nw_tos }"
+    info "  nw_proto: #{ message.match.nw_proto.to_hex }"
+    info "  nw_src: #{ message.match.nw_src.to_hex }"
+    info "  nw_dst: #{ message.match.nw_dst.to_hex }"
+    info "  tp_src: #{ message.match.tp_src }"
+    info "  tp_dst: #{ message.match.tp_dst }"
 
-    info "cookie: %#x" % message.cookie
-    info "priority: %u" % message.priority
-    info "reason: %#x" % message.reason
-    info "duration_sec: %u" % message.duration_sec
-    info "duration_nsec: %u" % message.duration_nsec
-    info "idle_timeout: %u" % message.idle_timeout
-    info "packet_count: %#x" % message.packet_count
-    info "byte_count: %#x" % message.byte_count
+    info "cookie: #{ message.cookie.to_hex }"
+    info "priority: #{ message.priority }"
+    info "reason: #{ message.reason.to_hex }"
+    info "duration_sec: #{ message.duration_sec }"
+    info "duration_nsec: #{ message.duration_nsec }"
+    info "idle_timeout: #{ message.idle_timeout }"
+    info "packet_count: #{ message.packet_count.to_hex }"
+    info "byte_count: #{ message.byte_count.to_hex }"
     run_next_event
   end
 
@@ -68,9 +68,9 @@ class Dumper < Controller
   def get_config_reply message
     info "[get_config_reply]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "flags: %#x" % message.flags
-    info "miss_send_len: %u" % message.miss_send_len
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "flags: #{ message.flags.to_hex }"
+    info "miss_send_len: #{ message.miss_send_len }"
     run_next_event
   end
 
@@ -84,8 +84,8 @@ class Dumper < Controller
   def port_status message
     info "[port status]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "reason: %#x" % message.reason
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "reason: #{ message.reason.to_hex }"
     dump_phy_port message.phy_port
   end
 
@@ -93,9 +93,9 @@ class Dumper < Controller
   def stats_reply message
     info "[stats_reply]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "type: %#x" % message.type
-    info "flags: %#x" % message.flags
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "type: #{ message.type.to_hex }"
+    info "flags: #{ message.flags.to_hex }"
     message.stats.each { | each | info each.to_s }
     run_next_event
   end
@@ -104,10 +104,10 @@ class Dumper < Controller
   def openflow_error message
     info "[error]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "type: %#x" % message.type
-    info "code: %#x" % message.code
-    info "data: %s", message.data.unpack( "H*" )
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "type: #{ message.type.to_hex }"
+    info "code: #{ message.code.to_hex }"
+    info "data: #{ message.data.unpack "H*" }"
     run_next_event
   end
 
@@ -115,17 +115,17 @@ class Dumper < Controller
   def queue_get_config_reply message
     info "[queue get_config_reply]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "port: %u" % message.port
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "port: #{ message.port }"
     info( "queues:" );
-    dump_packet_queue(message.queues)
+    dump_packet_queue message.queues
   end
 
 
   def barrier_reply message
     info "[barrier_reply]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
+    info "transaction_id: #{ message.transaction_id.to_hex }"
     run_next_event
   end
 
@@ -142,19 +142,21 @@ class Dumper < Controller
   def features_reply message
     info "[features_reply]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
-    info "n_buffers: %u" % message.n_buffers
-    info "n_tables: %u" % message.n_tables
-    info "capabilities: %#x" % message.capabilities
-    info "actions: %#x" % message.actions
-    message.ports.each { | each | dump_phy_port each }
+    info "transaction_id: #{ message.transaction_id.to_hex }"
+    info "n_buffers: #{ message.n_buffers }"
+    info "n_tables: #{ message.n_tables }"
+    info "capabilities: #{ message.capabilities.to_hex }"
+    info "actions: #{ message.actions.to_hex }"
+    message.ports.each do | each |
+      dump_phy_port each
+    end
   end
 
 
   def vendor message
     info "[vendor]"
     info "datapath_id: #{ message.datapath_id.to_hex }"
-    info "transaction_id: #{ transaction_id.to_hex }"
+    info "transaction_id: #{ message.transaction_id.to_hex }"
     info "data:"
     info message.buffer.unpack( "H*" )
   end
@@ -162,7 +164,7 @@ class Dumper < Controller
 
   def list_switches_reply dpids
     info "[list_switches_reply]"
-    info "switches = %s" % dpids.collect { | each | sprintf( "0x%x", each ) }.join( ", " )
+    info "switches = %s" % dpids.collect { | each | each.to_hex }.join( ", " )
     run_next_event
   end
 
@@ -175,26 +177,26 @@ class Dumper < Controller
   def dump_phy_port port
     # for testing port-status record the mac address if port.number == 2.
     @hw_addr = port.hw_addr if port.number == 2 
-    info "port_no: %u" % port.number
-    info "  hw_addr: #{port.hw_addr}"
-    info "  name: #{port.name}"
-    info "  config: %#x" % port.config
-    info "  state: %#x"  % port.state
-    info "  curr: %#x" % port.curr
-    info "  advertised: %#x" % port.advertised
-    info "  supported: %#x" % port.supported
-    info "  peer: %#x" % port.peer
+    info "port_no: #{ port.number }"
+    info "  hw_addr: #{ port.hw_addr }"
+    info "  name: #{ port.name }"
+    info "  config: #{ port.config.to_hex }"
+    info "  state: #{ port.state.to_hex }"
+    info "  curr: #{ port.curr.to_hex }"
+    info "  advertised: #{ port.advertised.to_hex }"
+    info "  supported: #{ port.supported.to_hex }"
+    info "  peer: #{ port.peer.to_hex }"
   end
 
 
   def dump_packet_queue queues
     queues.each do | packet_queue |
-      info "queue_id: %#x" % packet_queue.queue_id
-      info "  len: %u" % packet_queue.len
+      info "queue_id: #{ packet_queue.queue_id.to_hex }"
+      info "  len: #{ packet_queue.len }"
       info "  properties:"
       packet_queue.properties.each do | prop |
-        info "    property: %#x" % prop.property
-        info "    len: %#x" % prop.len
+        info "    property: #{ prop.property.to_hex }"
+        info "    len: #{ prop.len.to_hex }"
         info "      rate: %u" % prop.rate if prop.property == PacketQueue::OFPQT_MIN_RATE
       end
     end
