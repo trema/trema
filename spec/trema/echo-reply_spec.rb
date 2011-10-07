68005e7af9f08161996c24391f770cb68d0111d7
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-10-07 11:24:26 +0900

    added rspec for EchoReply

---
+++ spec/trema/echo-reply_spec.rb
@@ -0,0 +1,40 @@
+#
+# Author: Nick Karanatsios <nickkaranatsios@gmail.com>
+#
+# Copyright (C) 2008-2011 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
+require File.join( File.dirname( __FILE__ ), "..", "spec_helper" )
+require "trema"
+
+
+describe EchoReply, ".new" do
+  it_should_behave_like "any Openflow message with default transaction ID"
+end
+
+
+describe EchoReply, ".new( transaction_id )" do
+  subject { EchoReply.new transaction_id }
+  it_should_behave_like "any OpenFlow message"
+end
+
+
+### Local variables:
+### mode: Ruby
+### coding: utf-8-unix
+### indent-tabs-mode: nil
+### End:
