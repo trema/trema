d8d14c0ce44edf5d1863470e22dfc01a1824acc0
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-10-11 16:24:01 +0900

    add options hash as a command line argument

--- ruby/trema/echo-reply.c
+++ ruby/trema/echo-reply.c
@@ -34,14 +34,21 @@ echo_reply_alloc( VALUE klass ) {
 
 
 /*
- * Creates a {EchoReply} instance mainly used for testing
+ * Creates a {EchoReply} instance. This message is not a reply to
+ * +OFPT_ECHO_REQUEST+ but could be created by the user for testing.
  *
- * @overload initialize(transaction_id=nil)
- *   Creates a {EchoReply} object by specifying its transaction id. If
- *   transaction_id is not specified, an auto-generated transaction_id
- *   is set.
+ * @overload initialize(options={})
+ *
+ *   @example
+ *     EchoReply.new( :transaction_id => 123 )
+ *
+ *   @param [Hash] options the options hash.
+ *
+ *   @option options [Symbol] :transaction_id
+ *     An unsigned 32bit integer auto-generated if not supplied.
  *
  * @raise [ArgumentError] if transaction id is not an unsigned 32bit integer.
+ * @raise [TypeEror] if options is not a hash.
  *
  * @return [EchoReply] an object that encapsulates the +OFPT_ECHO_REPLY+ openflow message.
  */
@@ -49,17 +56,18 @@ static VALUE
 echo_reply_init( int argc, VALUE *argv, VALUE self ) {
   buffer *echo_reply;
   Data_Get_Struct( self, buffer, echo_reply );
-  
-  uint32_t xid;
-  VALUE xid_ruby;
-  if ( rb_scan_args( argc, argv, "01", &xid_ruby ) == 1 ) {
-    if ( rb_funcall( xid_ruby, rb_intern( "unsigned_32bit?" ), 0 ) == Qfalse ) {
-      rb_raise( rb_eArgError, "Transaction ID must be an unsigned 32bit integer" );
+  uint32_t xid = get_transaction_id( );
+  VALUE options;
+
+  if ( rb_scan_args( argc, argv, "01", &options ) == 1 ) {
+    Check_Type( options, T_HASH );
+    VALUE xid_ruby;
+    if ( ( xid_ruby = rb_hash_aref( options, ID2SYM( rb_intern( "transaction_id" ) ) ) ) != Qnil ) {
+      if ( rb_funcall( xid_ruby, rb_intern( "unsigned_32bit?" ), 0 ) == Qfalse ) {
+        rb_raise( rb_eArgError, "Transaction ID must be an unsigned 32bit integer" );
+      }
+      xid = ( uint32_t ) NUM2UINT( xid_ruby );
     }
-    xid = ( uint32_t ) NUM2UINT( xid_ruby );
-  } 
-  else {
-    xid = get_transaction_id();
   }
   ( ( struct ofp_header * ) ( echo_reply->data ) )->xid = htonl( xid );
   return self;
