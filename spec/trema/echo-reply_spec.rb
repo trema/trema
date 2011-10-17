66747ba4d0b31fba2cb2182fadbe4c54ea9acf2e
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-10-17 17:51:42 +0900

    refactor

--- spec/trema/echo-reply_spec.rb
+++ spec/trema/echo-reply_spec.rb
@@ -22,21 +22,21 @@ require File.join( File.dirname( __FILE__ ), "..", "spec_helper" )
 require "trema"
 
 
-describe EchoReply, ".new" do
+describe EchoReply, ".new( OPTIONAL OPTION MISSING )" do
   it_should_behave_like "any Openflow message with default transaction ID"
 end
 
 
-describe EchoReply, ".new( :transaction_id => value )" do
+describe EchoReply, ".new( VALID OPTION )" do
   subject { EchoReply.new :transaction_id => transaction_id }
-  it_should_behave_like "any OpenFlow message"
+  it_should_behave_like "any OpenFlow message with transaction_id option"
 end
 
 
-describe EchoReply, ".new( INVALID_OPTIONS )" do
-  it "should raise a TypeError" do
+describe EchoReply, ".new( INVALID OPTION )" do
+  it "should raise TypeError" do
     expect {
-      EchoReply.new "INVALID_OPTIONS"
+      EchoReply.new "INVALID OPTION"
     }.to raise_error( TypeError )
   end
 end
