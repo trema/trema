905f73b914b09273d9ccd24ad3a4288a40c1184b
Nick Karanatsios <nickkaranatsios@gmail.com>
2011-10-18 15:04:34 +0900

    change options to hash

--- ruby/trema/port-mod.c
+++ ruby/trema/port-mod.c
@@ -43,58 +43,96 @@ port_mod_alloc( VALUE kclass ) {
 /*
  * A {PortMod} instance a request object to perform operations on a physical port.
  *
- * @overload initialize(port_no, hw_addr, config, mask, advertise)
+ * @overload initialize(options={})
+ *   @example
+ *     PortMod.new(
+ *       :port_no => 1, 
+ *       :hw_addr => "11:22:33:44:55:66",
+ *       :config => 1,
+ *       :mask => 1,
+ *       :advertise => 0
+ *     )
  *
- *   @param [Number] port_no an index into datapath's ports list.
+ *   @param [Hash] options
+ *     the options to create a message with.
  *
- *   @param [String,Number,Trema::Mac] hw_addr the hardware address of a port.
+ *   @param [Number] :port_no 
+ *     an index into datapath's ports list.
+ *
+ *   @param [String,Number,Trema::Mac] :hw_addr
+ *     the hardware address of a port.
  *     Unique for each port. Obtained from +OFPT_FEATURES_REPLY+ message.
- *     Can be supplied as a string, number or as a Mac object. 
+ *     Can be supplied as a string, number or as a Mac object.
  *
- *   @param [Number] config a bitmap that can be set to configure a port.
+ *   @param [Number] :config 
+ *     a bitmap that can be set to configure a port.
  *
- *   @param [Number] mask set the bits of the +config+ flag to change.
+ *   @param [Number] :mask 
+ *     set the bits of the +config+ flag to change.
  *
- *   @param [Number] advertise bitmap of +ofp_port_features+ set to zero to prevent
+ *   @param [Number] :advertise
+ *     bitmap of +ofp_port_features+ set to zero to prevent
  *     any changes. Or can be copied from +OFPT_FEATURES_REPLY+ message.
  *
- * @return [PortMod]
- *   an object that encapsulates the +OFPT_PORT_MOD+ openflow message.
+ *   @return [PortMod]
+ *     an object that encapsulates the +OFPT_PORT_MOD+ OpenFlow message.
  */
 static VALUE
-port_mod_init( VALUE self,
-  VALUE port_no,
-  VALUE hw_addr,
-  VALUE config,
-  VALUE mask,
-  VALUE advertise ) {
-  buffer *port_mod;
-  VALUE mac = hw_addr;
-  uint32_t transaction_id = get_transaction_id();
+port_mod_init( int argc, VALUE *argv, VALUE self ) {
+  VALUE options;
 
-  Data_Get_Struct( self, buffer, port_mod );
-  if ( rb_obj_is_kind_of( hw_addr, rb_cString ) == Qtrue ||
-          rb_obj_is_kind_of( hw_addr, rb_cInteger ) == Qtrue ) {
-      mac = rb_funcall( rb_eval_string( "Trema::Mac" ), rb_intern( "new" ), 1, hw_addr );
-  } else if ( rb_obj_is_instance_of( hw_addr, rb_eval_string( "Trema::Mac" ) ) == Qfalse ) {
-    rb_raise( rb_eArgError, "hw_addr must be a string or an integer or Mac object" );
+  if ( rb_scan_args( argc, argv, "01", &options ) == 1 ) {
+    Check_Type( options, T_HASH );
+    buffer *port_mod;
+    Data_Get_Struct( self, buffer, port_mod );
+    VALUE mac;
+    VALUE hw_addr;
+    uint8_t *ptr;
+    uint8_t haddr[ OFP_ETH_ALEN ];
+
+    if ( ( hw_addr = rb_hash_aref( options, ID2SYM( rb_intern( "hw_addr" ) ) ) ) != Qnil ) {
+      mac = hw_addr;
+      if ( rb_obj_is_kind_of( hw_addr, rb_cString ) == Qtrue ||
+        rb_obj_is_kind_of( hw_addr, rb_cInteger ) == Qtrue ) {
+        mac = rb_funcall( rb_eval_string( "Trema::Mac" ), rb_intern( "new" ), 1, hw_addr );
+      } else if ( rb_obj_is_instance_of( hw_addr, rb_eval_string( "Trema::Mac" ) ) == Qfalse ) {
+        rb_raise( rb_eArgError, "hw_addr must be a string or an integer or Mac object" );
+      }
+      ptr = ( uint8_t* ) dl_addr_short( mac, haddr );
+      rb_iv_set( self, "@hw_addr", mac );
+    }
+    VALUE port_no;
+    if ( ( port_no = rb_hash_aref( options, ID2SYM( rb_intern( "port_no" ) ) ) ) != Qnil ) {
+      if ( rb_funcall( port_no, rb_intern( "unsigned_16bit?" ), 0 ) == Qfalse ) {
+        rb_raise( rb_eArgError, "Port number must be an unsigned 16-bit integer" );
+      }
+      rb_iv_set( self, "@port_no", port_no );
+    }
+    VALUE config;
+    if ( ( config = rb_hash_aref( options, ID2SYM( rb_intern( "config" ) ) ) ) != Qnil ){
+      rb_iv_set( self, "@config", config );
+    }
+    VALUE  mask;
+    if ( ( mask = rb_hash_aref( options, ID2SYM( rb_intern( "mask" ) ) ) ) != Qnil ){
+      rb_iv_set( self, "@mask", mask );
+    }
+    VALUE advertise;
+    if ( ( advertise = rb_hash_aref( options, ID2SYM( rb_intern( "advertise" ) ) ) ) != Qnil ){
+      rb_iv_set( self, "@advertise", advertise );
+    }
+    uint32_t xid = get_transaction_id();
+    rb_iv_set( self, "@transaction_id", UINT2NUM( xid ) );
+
+    ( ( struct ofp_header * ) ( port_mod->data ) )->xid = htonl( xid );
+    ( ( struct ofp_port_mod * ) ( port_mod->data ) )->port_no = htons( ( uint16_t ) NUM2UINT( port_no ) );
+    memcpy( ( ( struct ofp_port_mod * ) ( port_mod->data ) )->hw_addr, ptr, OFP_ETH_ALEN );
+    ( ( struct ofp_port_mod * ) ( port_mod->data ) )->config = htonl( ( uint32_t ) NUM2UINT( config ) );
+    ( ( struct ofp_port_mod * ) ( port_mod->data ) )->mask = htonl( ( uint32_t ) NUM2UINT( mask ) );
+    ( ( struct ofp_port_mod * ) ( port_mod->data ) )->advertise = htonl( ( uint32_t ) NUM2UINT( advertise ) );
   }
-  uint8_t *ptr;
-  uint8_t haddr[ OFP_ETH_ALEN ];
-  ptr = ( uint8_t* ) dl_addr_short( mac, haddr );
-  rb_iv_set( self, "@transaction_id", UINT2NUM( transaction_id ) );
-  rb_iv_set( self, "@port_no", port_no );
-  rb_iv_set( self, "@hw_addr", mac );
-  rb_iv_set( self, "@config", config );
-  rb_iv_set( self, "@mask", mask );
-  rb_iv_set( self, "@advertise", advertise );
-  
-  ( ( struct ofp_header * ) ( port_mod->data ) )->xid = htonl( transaction_id );
-  ( ( struct ofp_port_mod * ) ( port_mod->data ) )->port_no = htons( ( uint16_t ) NUM2UINT( port_no ) );
-  memcpy( ( ( struct ofp_port_mod * ) ( port_mod->data ) )->hw_addr, ptr, OFP_ETH_ALEN );
-  ( ( struct ofp_port_mod * ) ( port_mod->data ) )->config = htonl( ( uint32_t ) NUM2UINT( config ) );
-  ( ( struct ofp_port_mod * ) ( port_mod->data ) )->mask = htonl( ( uint32_t ) NUM2UINT( mask ) );
-  ( ( struct ofp_port_mod * ) ( port_mod->data ) )->advertise = htonl( ( uint32_t ) NUM2UINT( advertise ) );
+  else {
+    rb_raise( rb_eArgError, "Port no, hw_addr, config, mask, advertise are mandatory options" );
+  }
   return self;
 }
 
@@ -170,7 +208,7 @@ void
 Init_port_mod() {
   cPortMod = rb_define_class_under( mTrema, "PortMod", rb_cObject );
   rb_define_alloc_func( cPortMod, port_mod_alloc );
-  rb_define_method( cPortMod, "initialize", port_mod_init, 5 );
+  rb_define_method( cPortMod, "initialize", port_mod_init, -1 );
   rb_define_method( cPortMod, "transaction_id", port_mod_transaction_id, 0 );
   rb_define_method( cPortMod, "port_no", port_mod_port_no, 0 );
   rb_define_method( cPortMod, "hw_addr", port_mod_hw_addr, 0 );
