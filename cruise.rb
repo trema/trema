14016c5584bb536c7a9fa165e313e66df7d2dd95
Yasuhito Takamiya <yasuhito@gmail.com>
2011-11-24 11:05:47 +0900

    Merge INSTALL into README.md, and use bundler for cruise.rb environment.

--- cruise.rb
+++ cruise.rb
@@ -35,12 +35,14 @@ $coverage_threshold = 73.3
 
 $LOAD_PATH.unshift( File.expand_path( File.dirname( __FILE__ ) + "/ruby" ) )
 
+require "rubygems"
+require "bundler/setup"
+
 require "English"
 require "blocker"
 require "fileutils"
 require "find"
 require "optparse"
-require "rubygems"
 require "stringio"
 require "sub-process"
 require "trema/path"
