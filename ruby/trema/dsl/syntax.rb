b7951d993935acc9cb8a1883176017b82d936d0c
Yasuhito Takamiya <yasuhito@gmail.com>
2011-12-16 12:00:35 +0900

    Deleted ruby/trema/common-commands.rb

--- ruby/trema/dsl/syntax.rb
+++ ruby/trema/dsl/syntax.rb
@@ -20,17 +20,80 @@
 #
 
 
-require "trema/common-commands"
+require "trema/app"
+require "trema/dsl/app"
+require "trema/dsl/link"
+require "trema/dsl/switch"
+require "trema/dsl/vhost"
+require "trema/dsl/vswitch"
+require "trema/host"
+require "trema/link"
+require "trema/open-vswitch"
+require "trema/openflow-switch"
+require "trema/packetin-filter"
+require "trema/switch-manager"
+require "trema/tremashark"
 
 
 module Trema
   module DSL
     class Syntax
-      include Trema::CommonCommands
-
-
       def initialize context
         @context = context
+      end
+
+
+      def use_tremashark
+        @context.tremashark = Trema::Tremashark.new
+      end
+
+
+      def port number
+        @context.port = number
+      end
+
+
+      def link peer0, peer1
+        stanza = Trema::DSL::Link.new( peer0, peer1 )
+        Trema::Link.new( stanza )
+      end
+
+
+      def switch name = nil, &block
+        stanza = Trema::DSL::Switch.new( name )
+        stanza.instance_eval( &block )
+        Trema::OpenflowSwitch.new( stanza )
+      end
+
+
+      def vswitch name = nil, &block
+        stanza = Trema::DSL::Vswitch.new( name )
+        stanza.instance_eval( &block )
+        Trema::OpenVswitch.new stanza, @context.port
+      end
+
+
+      def vhost name = nil, &block
+        stanza = Trema::DSL::Vhost.new( name )
+        stanza.instance_eval( &block ) if block
+        Trema::Host.new( stanza )
+      end
+
+
+      def filter rule
+        Trema::PacketinFilter.new( rule )
+      end
+
+
+      def event rule
+        Trema::SwitchManager.new( rule, @context.port )
+      end
+
+
+      def app name = nil, &block
+        stanza = Trema::DSL::App.new( name )
+        stanza.instance_eval( &block )
+        Trema::App.new( stanza )
       end
     end
   end
