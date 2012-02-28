da7ca69044f863c1673fcc48cd4aec23551de513
Kazuya Suzuki <kazuya@ax.jp.nec.com>
2012-02-28 15:27:26 +0900

    Add igmp accessor tests into packet-in_spec.rb.

--- spec/trema/packet-in_spec.rb
+++ spec/trema/packet-in_spec.rb
@@ -100,91 +100,6 @@ describe Trema::PacketIn do
   end
 
   context "when reading packet content" do
-    it "should have correct VLAN and ICMP packet fields" do
-      network {
-        vswitch( "packet-in" ) { datapath_id 0xabc }
-        vhost "host1"
-        vhost ( "host2" ) {
-          ip "192.168.32.1"
-          netmask "255.255.255.0"
-          mac "00:00:00:00:00:02"
-        }
-        link "host1", "packet-in"
-        link "host2", "packet-in"
-      }.run( PacketInSendController ) {
-        data = [
-          0x00, 0x00, 0x00, 0x00, 0x00, 0x02, # dst
-          0x00, 0x00, 0x00, 0x00, 0x00, 0x01, # src
-          # vlan tag
-          0x81, 0x00, # tpid
-          0x0f, 0x9f, # tci
-          0x08, 0x00, # ether type
-          # ipv4
-          0x45, 0x00, # version
-          0x00, 0x3c, # length
-          0x8c, 0x1b,
-          0x00, 0x00,
-          0x80,       # ttl
-          0x01,       # protocol
-          0xed, 0x09, # checksum
-          0xc0, 0xa8, 0x20, 0x4a, # src
-          0xc0, 0xa8, 0x20, 0x01, # dst
-          # icmp
-          0x08, # type
-          0x00, # code
-          0xe9, 0x5b, # checksum
-          0x04, 0x00, # id
-          0x60, 0x00, # seq
-          0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
-          0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
-          0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
-          0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69
-        ].pack( "C*" )
-        controller( "PacketInSendController" ).should_receive( :packet_in ) do | datapath_id, message | 
-          message.in_port.should > 0
-          message.vtag?.should be_true
-          message.arp?.should be_false
-          message.ipv4?.should be_true
-          message.udp?.should be_false
-          message.tcp?.should be_false
-          message.icmpv4?.should be_true
-          message.igmp?.should be_false
-
-          message.vlan_tpid.should == 0x8100
-          message.vlan_tci.should == 0x0f9f
-          message.vlan_prio.should == 0
-          message.vlan_cfi.should == 0
-          message.vlan_vid.should == 0xf9f
-          message.eth_type.should == 0x0800
-
-          message.ipv4_version.should == 4
-          message.ipv4_ihl.should == 5
-          message.ipv4_tos.should == 0
-          message.ipv4_tot_len.should == 0x003c
-          message.ipv4_id.should == 0x8c1b
-          message.ipv4_frag_off.should == 0
-          message.ipv4_ttl.should == 0x80
-          message.ipv4_protocol.should == 1
-          message.ipv4_checksum.should == 0xed09
-          message.ipv4_saddr.to_s.should == "192.168.32.74"
-          message.ipv4_daddr.to_s.should == "192.168.32.1"
-
-          message.icmpv4_type.should == 8
-          message.icmpv4_code.should == 0
-          message.icmpv4_checksum.should == 0xe95b
-          message.icmpv4_id.should == 0x0400
-          message.icmpv4_seq.should == 0x6000
-        end
-
-        controller( "PacketInSendController" ).send_packet_out(
-          0xabc,
-          :data => data,
-          :actions => Trema::ActionOutput.new( :port => Controller::OFPP_CONTROLLER )
-        )
-        sleep 2
-      }
-    end
-
     it "should have correct ARP packet fields" do
       network {
         vswitch( "packet-in" ) { datapath_id 0xabc }
@@ -381,6 +296,165 @@ describe Trema::PacketIn do
           message.udp_dst_port.should == 2
           message.udp_checksum.should == 0
           message.udp_len.should == 0x1e
+        end
+
+        controller( "PacketInSendController" ).send_packet_out(
+          0xabc,
+          :data => data,
+          :actions => Trema::ActionOutput.new( :port => Controller::OFPP_CONTROLLER )
+        )
+        sleep 2
+      }
+    end
+
+    it "should have correct VLAN and ICMPv4 packet fields" do
+      network {
+        vswitch( "packet-in" ) { datapath_id 0xabc }
+        vhost "host1"
+        vhost ( "host2" ) {
+          ip "192.168.32.1"
+          netmask "255.255.255.0"
+          mac "00:00:00:00:00:02"
+        }
+        link "host1", "packet-in"
+        link "host2", "packet-in"
+      }.run( PacketInSendController ) {
+        data = [
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x02, # dst
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x01, # src
+          # vlan tag
+          0x81, 0x00, # tpid
+          0x0f, 0x9f, # tci
+          0x08, 0x00, # ether type
+          # ipv4
+          0x45, 0x00, # version
+          0x00, 0x3c, # length
+          0x8c, 0x1b,
+          0x00, 0x00,
+          0x80,       # ttl
+          0x01,       # protocol
+          0xed, 0x09, # checksum
+          0xc0, 0xa8, 0x20, 0x4a, # src
+          0xc0, 0xa8, 0x20, 0x01, # dst
+          # icmp
+          0x08, # type
+          0x00, # code
+          0xe9, 0x5b, # checksum
+          0x04, 0x00, # id
+          0x60, 0x00, # seq
+          0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+          0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
+          0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
+          0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69
+        ].pack( "C*" )
+        controller( "PacketInSendController" ).should_receive( :packet_in ) do | datapath_id, message | 
+          message.in_port.should > 0
+          message.vtag?.should be_true
+          message.arp?.should be_false
+          message.ipv4?.should be_true
+          message.udp?.should be_false
+          message.tcp?.should be_false
+          message.icmpv4?.should be_true
+          message.igmp?.should be_false
+
+          message.vlan_tpid.should == 0x8100
+          message.vlan_tci.should == 0x0f9f
+          message.vlan_prio.should == 0
+          message.vlan_cfi.should == 0
+          message.vlan_vid.should == 0xf9f
+          message.eth_type.should == 0x0800
+
+          message.ipv4_version.should == 4
+          message.ipv4_ihl.should == 5
+          message.ipv4_tos.should == 0
+          message.ipv4_tot_len.should == 0x003c
+          message.ipv4_id.should == 0x8c1b
+          message.ipv4_frag_off.should == 0
+          message.ipv4_ttl.should == 0x80
+          message.ipv4_protocol.should == 1
+          message.ipv4_checksum.should == 0xed09
+          message.ipv4_saddr.to_s.should == "192.168.32.74"
+          message.ipv4_daddr.to_s.should == "192.168.32.1"
+
+          message.icmpv4_type.should == 8
+          message.icmpv4_code.should == 0
+          message.icmpv4_checksum.should == 0xe95b
+          message.icmpv4_id.should == 0x0400
+          message.icmpv4_seq.should == 0x6000
+        end
+
+        controller( "PacketInSendController" ).send_packet_out(
+          0xabc,
+          :data => data,
+          :actions => Trema::ActionOutput.new( :port => Controller::OFPP_CONTROLLER )
+        )
+        sleep 2
+      }
+    end
+
+    it "should have correct IGMP packet fields" do
+      network {
+        vswitch( "packet-in" ) { datapath_id 0xabc }
+        vhost "host1"
+        vhost "host2"
+        link "host1", "packet-in"
+        link "host2", "packet-in"
+      }.run( PacketInSendController ) {
+        data = [
+          0x01, 0x00, 0x5e, 0x00, 0x00, 0x01, # dst
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x01, # src
+          0x08, 0x00, # ether type
+          # ipv4
+          0x46, 0xc0, # version
+          0x00, 0x20, # length
+          0x3a, 0xea,
+          0x00, 0x00,
+          0x01,       # ttl
+          0x02,       # protocol
+          0xe4, 0x58, # checksum
+          0xc0, 0xa8, 0x64, 0x2b, # src
+          0xe0, 0x00, 0x00, 0x01, # dst
+          0x94, 0x04, 0x00, 0x00,
+          # igmp
+          0x11, # type
+          0x64, # code
+          0xee, 0x9b, # checksum
+          0x00, 0x00, 0x00, 0x00,
+          0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+          0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
+          0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
+          0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69
+        ].pack( "C*" )
+        controller( "PacketInSendController" ).should_receive( :packet_in ) do | datapath_id, message | 
+          message.in_port.should > 0
+          message.vtag?.should be_false
+          message.arp?.should be_false
+          message.ipv4?.should be_true
+          message.udp?.should be_false
+          message.tcp?.should be_false
+          message.icmpv4?.should be_false
+          message.igmp?.should be_true
+
+          message.igmp_membership_query?.should be_true
+          message.igmp_v1_membership_report?.should be_false
+          message.igmp_v2_membership_report?.should be_false
+          message.igmp_v2_leave_group?.should be_false
+          message.igmp_v3_membership_report?.should be_false
+
+          message.ipv4_version.should == 4
+          message.ipv4_ihl.should == 6
+          message.ipv4_tos.should == 0xc0
+          message.ipv4_tot_len.should == 0x0020
+          message.ipv4_id.should == 0x3aea
+          message.ipv4_frag_off.should == 0
+          message.ipv4_ttl.should == 1
+          message.ipv4_protocol.should == 2
+          message.ipv4_checksum.should == 0xe458
+          message.ipv4_saddr.to_s.should == "192.168.100.43"
+          message.ipv4_daddr.to_s.should == "224.0.0.1"
+
+          message.igmp_type.should == 0x11
+          message.igmp_checksum.should == 0xee9b
         end
 
         controller( "PacketInSendController" ).send_packet_out(
