f9676325add48dba6ad93e1ff92523b3a7f93a37
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2012-07-01 12:59:36 +0900

    OpenFlowPort supported

--- src/examples/active_record/active_record_app.rb
+++ src/examples/active_record/active_record_app.rb
@@ -17,8 +17,20 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
+# build following association 
+#   +---------------+ 1    N +------------+
+#   | OpenFlowSwitch| <----> |OpenFlowPort|
+#   +---------------+        +------------+
+#
+
 # ActiveRecord data model as OpenFlowSwitch
 class OpenFlowSwitch < ActiveRecord::Base
+  has_many :open_flow_ports, :dependent => :destroy
+end
+
+# ActiveRecord data model as OpenFlowPort
+class OpenFlowPort < ActiveRecord::Base
+  belongs_to :open_flow_switch
 end
 
 # create or delete table for open_flow_switches
@@ -41,6 +53,27 @@ class CreateOpenFlowSwitch < ActiveRecord::Migration
   end
 end
 
+class CreateOpenFlowPort < ActiveRecord::Migration
+
+  TABLE = :open_flow_ports
+
+  #create table
+  def up
+    return if OpenFlowPort.table_exists?
+    create_table TABLE do |t|
+      t.integer :open_flow_switch_id #for associating to OpenFlowSwitch
+      t.integer :no                  #port number
+    end
+  end
+
+  #delete table
+  def down
+    return unless OpenFlowPort.table_exists?
+    drop_table TABLE
+  end
+end
+
+
 class DBConnector
   def self.start
     ActiveRecord::Base.establish_connection(
@@ -57,17 +90,32 @@ class MyController < Controller
   def start
     DBConnector.start
     CreateOpenFlowSwitch.new.up
+    CreateOpenFlowPort.new.up
   end
   
   def switch_ready dpid
-    return if OpenFlowSwitch.find_by_dpid(dpid)
-    OpenFlowSwitch.create(:dpid => dpid)
-    puts "controller gets switch #{dpid.to_hex}"
+    send_message dpid, FeaturesRequest.new
+  end
+  
+  def features_reply message
+    return if OpenFlowSwitch.find_by_dpid(message.datapath_id)
+    puts "controller gets switch #{message.datapath_id.to_hex}"
+
+    OpenFlowSwitch.transaction do
+      sw = OpenFlowSwitch.create(:dpid => message.datapath_id)
+      
+      message.ports.each do |trema_port|
+        sw.open_flow_ports << OpenFlowPort.create(:no => trema_port.number)
+      end
+    end
   end
 
   def list_switches
     OpenFlowSwitch.all.each do |sw|
       puts "list switch #{sw.dpid}"
+      sw.open_flow_ports.each do |port|
+        puts "  port[#{port.no}]"
+      end
     end
   end
 end
