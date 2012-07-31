1f0263e74a9a5a9048eceb00cb3340c23d3fd8b2
Kazuya Suzuki <kazuya@ax.jp.nec.com>
2012-07-31 10:30:42 +0900

    Implement switch-daemon's options with mutiple controllers.

--- spec/trema/switch-daemon_spec.rb
+++ spec/trema/switch-daemon_spec.rb
@@ -23,10 +23,52 @@ require "trema/switch-daemon"
 module Trema
   describe SwitchDaemon do
     it "should be initialized with proper options" do
-      rule = { :port_status => "topology", :packet_in => "controller", :state_notify => "topology", :vendor => "controller" }
+      rule = { 
+        :port_status => "topology", 
+        :packet_in => "controller", 
+        :state_notify => "topology", 
+        :vendor => "controller" 
+      }
       switch_daemon = SwitchDaemon.new( rule )
 
-      switch_daemon.options.should == [ "port_status::topology", "packet_in::controller", "state_notify::topology", "vendor::controller" ]
+      switch_daemon.options.should include( "port_status::topology" )
+      switch_daemon.options.should include( "packet_in::controller" ) 
+      switch_daemon.options.should include( "state_notify::topology" ) 
+      switch_daemon.options.should include( "vendor::controller" )
+    end
+
+
+    it "should be initialized with options which have multiple controllers" do
+      rule = { 
+        :port_status => "topology0,topology1", 
+        :packet_in => "controller0,controller1", 
+        :state_notify => "topology0,topology1", 
+        :vendor => "controller0,controller1" 
+      }
+      switch_daemon = SwitchDaemon.new( rule )
+
+      switch_daemon.options.should include( "port_status::topology0" ) 
+      switch_daemon.options.should include( "port_status::topology1" )
+      switch_daemon.options.should include( "packet_in::controller0" )
+      switch_daemon.options.should include( "packet_in::controller1" ) 
+      switch_daemon.options.should include( "state_notify::topology0" )
+      switch_daemon.options.should include( "state_notify::topology1" ) 
+      switch_daemon.options.should include( "vendor::controller0" ) 
+      switch_daemon.options.should include( "vendor::controller1" )
+    end
+
+
+    it "should be initialized without vendor options" do
+      rule = { 
+        :port_status => "topology", 
+        :packet_in => "controller", 
+        :state_notify => "topology" 
+      }
+      switch_daemon = SwitchDaemon.new( rule )
+
+      switch_daemon.options.should include( "port_status::topology" )
+      switch_daemon.options.should include( "packet_in::controller" ) 
+      switch_daemon.options.should include( "state_notify::topology" ) 
     end
   end
 end
