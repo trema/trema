4259863c47a6becf4a012a53d46596610dabe9b7
Kazuya Suzuki <kazuya@ax.jp.nec.com>
2012-07-31 11:03:54 +0900

    Remove unwanted spaces and Author tag.

--- ruby/trema/switch-daemon.rb
+++ ruby/trema/switch-daemon.rb
@@ -1,8 +1,6 @@
 #
 # The controller class of switch daemon.
 #
-# Author: Yasuhito Takamiya <yasuhito@gmail.com>
-#
 # Copyright (C) 2008-2012 NEC Corporation
 #
 # This program is free software; you can redistribute it and/or modify
