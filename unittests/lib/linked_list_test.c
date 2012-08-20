2fdf10c080d33e1754f7148fd05e36f5c672ca6a
gakusei <gakusei@debian.localhost>
2012-08-20 20:22:29 +0900

    Added unit-test description of iterate_list

--- unittests/lib/linked_list_test.c
+++ unittests/lib/linked_list_test.c
@@ -281,6 +281,74 @@ test_list_length_of_empty_list() {
 }
 
 
+typedef struct {
+  const char *value;
+  int counter;
+} list_data;
+
+static bool
+linear_procesisng( void *data, void *user_data ) {
+  assert_true( data != NULL );
+  assert_true( user_data != NULL );
+
+  list_data *d = ( list_data * )data;
+  int *counter = ( int * )user_data;
+  d->counter = ++( *counter );
+
+  if ( *counter == 1 ) {
+    assert_string_equal( d->value, "charlie" );
+  } else if ( *counter == 2 ) {
+    assert_string_equal( d->value, "bravo" );
+  } else if ( *counter == 3 ) {
+    assert_string_equal( d->value, "alpha" );
+  }
+
+  return false;
+}
+
+static bool
+extract_element_data( void *data, void *user_data ) {
+  assert_true( data != NULL );
+  assert_true( user_data == NULL );
+
+  bool ret = false;
+  list_data *d = ( list_data * )data;
+
+  if ( d->counter == 2 ) {
+    ret = true;
+  }
+
+  assert_true( d->counter < 3 );
+
+  return ret;
+}
+
+static void
+test_iterate_list() {
+  create_list( &new_list );
+  void *retdata;
+  int counter = 0;
+
+  list_data data_alpha = { .value = alpha, .counter = 0 };
+  list_data data_bravo = { .value = bravo, .counter = 0 };
+  list_data data_charlie = { .value = charlie, .counter = 0 };
+
+  insert_in_front( &new_list, &data_alpha );
+  insert_in_front( &new_list, &data_bravo );
+  insert_in_front( &new_list, &data_charlie );
+
+  retdata = iterate_list( new_list, linear_processing, &counter );
+  assert_true( retdata == NULL );
+  assert_int_equal( 3, counter );
+
+  retdata = iterate_list( new_list, extract_element_data, NULL );
+  assert_true( retdata != NULL );
+  assert_string_equal( ( ( list_data * )( retdata ) )->value, "bravo" );
+
+  delete_list( new_list );
+}
+
+
 static void
 test_delete_list() {
   create_list( &new_list );
@@ -327,6 +395,8 @@ main() {
 
     unit_test( test_list_length ),
     unit_test( test_list_length_of_empty_list ),
+
+    unit_test( test_iterate_list ),
 
     unit_test( test_delete_list ),
   };
