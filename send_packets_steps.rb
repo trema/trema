b6db234db43dc20749c0a5e91290324a26f57855
Yasuhito Takamiya <yasuhito@gmail.com>
2012-12-25 16:55:44 +0900

    Refactor features/step_definitions/send_packets_steps.rb (refs #222)

--- send_packets_steps.rb
+++ send_packets_steps.rb
@@ -16,26 +16,13 @@
 #
 
 
-When /^I run trema send_packets "([^"]*)"$/ do | args |
-  step "I run `trema send_packets #{ args }`"
-  sleep 1
-end
-
-
 When /^I send (\d+) packets from (.+) to (.+)$/ do | n_packets, host_a, host_b |
-  run "trema send_packets --source #{ host_a } --dest #{ host_b } --n_pkts #{ n_packets }"
-  sleep 5  # ensure that all packets are sent
+  step "I run `trema send_packets --source #{ host_a } --dest #{ host_b } --n_pkts #{ n_packets }`"
 end
 
 
 When /^I send 1 packet from (.+) to (.+)$/ do | host_a, host_b |
   step "I send 1 packets from #{ host_a } to #{ host_b }"
-end
-
-
-When /^I send packets from (.+) to (.+) \(duration = (\d+)\)$/ do | host_a, host_b, duration |
-  run "./trema send_packets --source #{ host_a } --dest #{ host_b } --duration #{ duration }"
-  sleep 1  # ensure that all packets are sent
 end
 
 
