c443745b49673f7599d19f3e63834d60bf08f33e
Yasuhito Takamiya <yasuhito@gmail.com>
2013-01-21 10:11:45 +0900

    Refactoring set-vlan-priority_spec.rb (refs #236)

--- spec/trema/set-vlan-priority_spec.rb
+++ spec/trema/set-vlan-priority_spec.rb
@@ -20,39 +20,38 @@ require File.join( File.dirname( __FILE__ ), "..", "spec_helper" )
 require "trema"
 
 
-describe SetVlanPriority, ".new( number )" do
+describe SetVlanPriority, ".new(vlan_priority)", :type => "actions" do
   subject { SetVlanPriority.new( vlan_priority ) }
 
-  context "when vlan_priority == 4" do
+  context "with vlan_priority (4)" do
     let( :vlan_priority ) { 4 }
     its( :vlan_priority ) { should == 4 }
   end
 
   it_validates "option range", :vlan_priority, 0..7
-end
 
+  context %{with vlan_priority ("4")} do
+    let( :vlan_priority ) { "4" }
+    it { expect { subject }.to raise_error( TypeError ) }
+  end
 
-describe SetVlanPriority, %{.new( "4" )} do
-  it { expect { SetVlanPriority.new( "4" ) }.to raise_error( TypeError ) }
-end
+  context "with vlan_priority ([4])" do
+    let( :vlan_priority ) { [ 4 ] }
+    it { expect { subject }.to raise_error( TypeError ) }
+  end
 
+  context "when sending a Flow Mod with SetVlanVid" do
+    let( :vlan_priority ) { 7 }
 
-describe SetVlanPriority, ".new( [ 4 ] )" do
-  it { expect { SetVlanPriority.new( [ 4 ] ) }.to raise_error( TypeError ) }
-end
-
-
-describe SetVlanPriority, ".new( VALID OPTION )" do
-  context "when sending #flow_mod(add) with action set to mod_vlan_pcp" do
-    it "should have a flow with action set to mod_vlan_pcp" do
-      class FlowModAddController < Controller; end
+    it "should insert a new flow with action (mod_vlan_pcp:7)" do
+      class TestController < Controller; end
       network {
         vswitch { datapath_id 0xabc }
-      }.run( FlowModAddController ) {
-        controller( "FlowModAddController" ).send_flow_mod_add( 0xabc, :actions => SetVlanPriority.new( 7 ) )
-        sleep 2 # FIXME: wait to send_flow_mod_add
+      }.run( TestController ) {
+        controller( "TestController" ).send_flow_mod_add( 0xabc, :actions => subject )
+        sleep 2
         vswitch( "0xabc" ).should have( 1 ).flows
-        vswitch( "0xabc" ).flows[0].actions.should match( /mod_vlan_pcp:7/ )
+        vswitch( "0xabc" ).flows[ 0 ].actions.should == "mod_vlan_pcp:7"
       }
     end
   end
