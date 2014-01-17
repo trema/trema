c239255db9b24273466f89c013792b99faf79c43
Yasuhito Takamiya <yasuhito@gmail.com>
2014-01-17 09:57:45 +0900

    Refactored to pass rubocop check (refs #342).

--- src/examples/transparent_firewall/block-rfc1918.rb
+++ src/examples/transparent_firewall/block-rfc1918.rb
@@ -16,22 +16,23 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 class BlockRFC1918 < Controller
-  def start
-    @dpid = nil
-    @prefixes = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
-    @prefixes.map! { |each| Pio::IPv4Address.new(each) }
+  PORTS = {
+    outside: 1,
+    inside: 2,
+    inspect: 3
+  }
+
+  PREFIXES = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'].map do |each|
+    Pio::IPv4Address.new each
   end
 
   def switch_ready(dpid)
-    unless @dpid.nil?
+    if @dpid
       info "#{dpid.to_hex}: ignored"
       return
     end
     @dpid = dpid
     info "#{@dpid.to_hex}: connected"
-    @outside_port_no = 1
-    @inside_port_no = 2
-    @inspect_port_no = 3
     start_loading
   end
 
@@ -41,39 +42,45 @@ class BlockRFC1918 < Controller
     @dpid = nil
   end
 
+  def barrier_reply(dpid, message)
+    return if dpid != @dpid
+    info "#{@dpid.to_hex}: loading finished"
+  end
+
+  private
+
   def start_loading
-    @prefixes.each do |each|
-      block_prefix_on_port(each, @inside_port_no, 5000)
-      block_prefix_on_port(each, @outside_port_no, 4000)
+    PREFIXES.each do |each|
+      block_prefix_on_port(each, PORTS[:inside], 5000)
+      block_prefix_on_port(each, PORTS[:outside], 4000)
     end
     install_postamble(1500)
     send_message(@dpid, BarrierRequest.new)
   end
 
-  def block_prefix_on_port(pfx, port_no, prio)
+  def block_prefix_on_port(prefix, port_number, priority)
     send_flow_mod_add(
-      @dpid, priority: prio + 100,
-             match: Match.new(in_port: port_no, dl_type: 0x0800, nw_src: pfx),
-             actions: [SendOutPort.new(@inspect_port_no)])
+      @dpid,
+      priority: priority + 100,
+      match: Match.new(in_port: port_number, dl_type: 0x0800, nw_src: prefix),
+      actions: SendOutPort.new(PORTS[:inspect]))
     send_flow_mod_add(
-      @dpid, priority: prio,
-             match: Match.new(in_port: port_no, dl_type: 0x0800, nw_dst: pfx),
-             actions: [SendOutPort.new(@inspect_port_no)])
+      @dpid,
+      priority: priority,
+      match: Match.new(in_port: port_number, dl_type: 0x0800, nw_dst: prefix),
+      actions: SendOutPort.new(PORTS[:inspect]))
   end
 
-  def install_postamble(prio)
+  def install_postamble(priority)
     send_flow_mod_add(
-      @dpid, priority: prio + 100,
-             match: Match.new(in_port: @inside_port_no),
-             actions: [SendOutPort.new(@outside_port_no)])
+      @dpid,
+      priority: priority + 100,
+      match: Match.new(in_port: PORTS[:inside]),
+      actions: SendOutPort.new(PORTS[:outside]))
     send_flow_mod_add(
-      @dpid, priority: prio,
-             match: Match.new(in_port: @outside_port_no),
-             actions: [SendOutPort.new(@inside_port_no)])
-  end
-
-  def barrier_reply(dpid, message)
-    return unless dpid == @dpid
-    info "#{@dpid.to_hex}: loading finished"
+      @dpid,
+      priority: priority,
+      match: Match.new(in_port: PORTS[:outside]),
+      actions: SendOutPort.new(PORTS[:inside]))
   end
 end
