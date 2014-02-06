748821c4f19fb6d2163e665b518b875245352583
SUGYO <sugyo@pb.jp.nec.com>
2014-02-06 16:08:00 +0900

    Change the timeout to 10 seconds

--- misc_steps.rb
+++ misc_steps.rb
@@ -31,7 +31,7 @@ When(/^wait until "([^"]*)" is up$/) do | process |
   pid_file = File.join(Trema.pid, "#{ process }.pid")
   loop do
     nloop += 1
-    fail 'Timeout' if nloop > 60 # FIXME
+    fail 'Timeout' if nloop > 100 # FIXME
     break if FileTest.exists?(pid_file) && !ps_entry_of(process).nil?
     sleep 0.1
   end
