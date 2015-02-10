bb8cb7e5e02f76cbae6e7660e7c35d506e33d61e
Yasuhito Takamiya <yasuhito@gmail.com>
2015-02-10 09:06:54 +0900

    Update dependencies.

--- send_packets_steps.rb
+++ send_packets_steps.rb
@@ -16,11 +16,11 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-When(/^I send (\d+) packets from (.+) to (.+)$/) do | n_packets, host_a, host_b |
+When(/^I send (\d+) packets from (.+) to (.+)$/) do |n_packets, host_a, host_b|
   step "I run `trema send_packets --source #{ host_a } --dest #{ host_b } --n_pkts #{ n_packets }`"
 end
 
-When(/^I send 1 packet from (.+) to (.+)$/) do | host_a, host_b |
+When(/^I send 1 packet from (.+) to (.+)$/) do |host_a, host_b|
   step "I send 1 packets from #{ host_a } to #{ host_b }"
 end
 
