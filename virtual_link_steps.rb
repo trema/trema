3e24e006815292a0a2b51853cd166aabe0a0d2ee
Yasuhito Takamiya <yasuhito@gmail.com>
2015-03-13 15:06:13 +0900

    Merge commit 'f9feecd8031cc613d3d48798828e7787ba1b6c91' into develop
    Conflicts:
    	features/step_definitions/virtual_link_steps.rb

--- virtual_link_steps.rb
+++ virtual_link_steps.rb
@@ -1,4 +1,8 @@
+# rubocop:disable LineLength
+
 Then(/^virtual links should not exist$/) do
-  step %Q(I run `bash -c 'ifconfig | grep "^link[0-9]\+-[0-9]\+" > virtual_links.txt'`)
+  step %(I run `bash -c 'ifconfig | grep "^link[0-9]\+-[0-9]\+" > virtual_links.txt'`)
   step 'the file "virtual_links.txt" should not contain "link"'
 end
+
+# rubocop:enable LineLength
