03a92770cfb7c9529608df0a24507761c666f888
Yasuhito Takamiya <yasuhito@gmail.com>
2015-03-13 16:36:22 +0900

    Fix rubocop errors.

--- dump_flows_steps.rb
+++ dump_flows_steps.rb
@@ -1,3 +1,5 @@
+# rubocop:disable LineLength
+
 Then(/^the switch "(.*?)" has (\d+) flow entr(?:y|ies)$/) do |switch, num_entries|
   command = "trema dump_flows #{switch} -S."
   step "I run `#{command}`"
@@ -7,3 +9,5 @@ end
 Then(/^the switch "(.*?)" has no flow entry$/) do |switch|
   step %(the switch "#{switch}" has 0 flow entry)
 end
+
+# rubocop:enable LineLength
