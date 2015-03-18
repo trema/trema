c394fc2988506b361a88ca40da75d688c30f6afd
Yasuhito Takamiya <yasuhito@gmail.com>
2015-03-18 10:07:00 +0900

    Phut 0.3.1.

--- show_stats_steps.rb
+++ show_stats_steps.rb
@@ -53,4 +53,31 @@ Then(/^the number of packets received by "(.*?)" should be:$/) do |host_name, ta
   end
 end
 
+Then(/^the total number of received packets should be:$/) do |table|
+  table.hashes[0].each_pair do |host_name, npackets|
+    command = "trema show_stats #{host_name}"
+    step "I run `#{command}`"
+
+    result = 0
+    in_current_dir do
+      received = false
+      output_from(command).split("\n").each do |each|
+        case each
+        when /Packets sent/
+          next
+        when /Packets received/
+          received = true
+          next
+        when /(\S+) -> (\S+) = (\d+) packet/
+          next unless received
+          result += Regexp.last_match(3).to_i
+        else
+          fail "Failed to parse line '#{each}'"
+        end
+      end
+    end
+    expect(result).to eq(npackets.to_i)
+  end
+end
+
 # rubocop:enable LineLength
