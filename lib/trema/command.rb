f697b5d7989eb15f10802ae1cda68554dd1f30c1
Masashi Honma <masashi.honma@gmail.com>
2015-09-03 17:39:17 +0900

    Add SSL support for socket between controller and switch
    --- How to use ---
    [create certificates with Open vSwitch]
    This command will create
    	$ sudo ovs-pki init
    Controller CA cert and
    	/var/lib/openvswitch/pki/controllerca/cacert.pem
    Switch CA cert.
    	/var/lib/openvswitch/pki/switchca/cacert.pem
    
    This command will create certificates for the Controller ./ctl-cert.pem and
    ./ctl-privkey.pem.
    $ sudo ovs-pki req+sign ctl controller
    
    This command will create certificates for the Switch ./sc-cert.pem and
    ./sc-privkey.pem.
    $ sudo ovs-pki req+sign sc switch
    
    [Configure Open vSwitch]
    Configure Open vSwitch by this command.
    $ sudo ovs-vsctl set-ssl <path to sc-privkey.pem> <path to sc-cert.pem> \
    <path to Controller CA cert>
    $ sudo ovs-vsctl set-controller br0 ssl:<hostname>:<portno>
    
    [Configure trema]
    Copy the ctl-cert.pem, ctl-privkey.pem and Switch CA cert to PC running trema.
    
    Run trema by this command.
    $ bundle exec ruby ./bin/trema -v run <path to controller file> \
    --ctl_privkey=<path to ctl-privkey.pem> --ctl_cert=<path to ctl-cert.pem> \
    --ca_certs=<path to Switch CA cert>
    
    ------------------
    The new trema argument is base on Ryu SSL argument.
    $ ryu-manager --ctl-privkey <path to ctl-privkey.pem> \
    --ctl-cert <path to ctl-cert.pem> --ca-certs <path to Switch CA cert>
    
    These commands are tested on this environment.
    trema
    	Ubuntu: 15.04 x64
    switch
    	Ubuntu: 14.04 x64
    	Open vSwitch: 2.0.2
    
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- lib/trema/command.rb
+++ lib/trema/command.rb
@@ -35,6 +35,9 @@ module Trema
         raise(InvalidLoggingLevel,
               "Invalid logging level: #{options[:logging_level]}")
       end
+      Controller.ctl_privkey = options[:ctl_privkey]
+      Controller.ctl_cert = options[:ctl_cert]
+      Controller.ca_certs = options[:ca_certs]
 
       $LOAD_PATH.unshift File.expand_path(File.dirname(@args.first))
       load @args.first
