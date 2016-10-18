e2b70e97fe03d92ed059d503963ccecfc47143e9
Yasuhito Takamiya <yasuhito@gmail.com>
2016-10-18 17:52:01 +0900

    Fix ArgumentError

--- lib/trema/command.rb
+++ lib/trema/command.rb
@@ -115,7 +115,7 @@ module Trema
     def start_phut
       return unless @options[:conf]
       system 'sudo -v'
-      @phut = Phut::Parser.new(Trema.logger).parse(@options[:conf])
+      @phut = Phut::Parser.new(@options[:conf]).parse
       @phut_run_thread = Thread.start { @phut.run }
       @phut_run_thread.join
       Thread.start { start_sudo_credential_update }
